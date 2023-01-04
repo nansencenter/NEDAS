1630aa53b1479d57134caed4b22efbd3bc1c29e0
Yue Ying <hardyying@gmail.com>
2023-01-04 14:57:33 +0100

    update correlation and spectrum plots

--- perturbation/fig_err_spec.py
+++ perturbation/fig_err_spec.py
@@ -8,29 +8,40 @@ cmap = [plt.cm.jet(m) for m in np.linspace(0.2, 0.8, 44)]
 
 ke_spec = np.load(pert_path+'sample_ECMWF/ke_spec.npy')
 n_sample, nk, nt = ke_spec.shape
-
 dt = 4
 for t in np.arange(1, nt):
-    wn = np.arange(1., 30.)
-    pwr = np.mean(ke_spec[:, :, t], axis=0)
-    ax.loglog(wn, pwr[1:30], color=cmap[t*dt][0:3])
+    wn = np.arange(1., 20.)
+    pwr = np.mean(ke_spec[:, :, t], axis=0)/2.
+    ax.loglog(wn, pwr[1:20], color=cmap[t*dt][0:3])
 
-# ke_spec = np.load(pert_path+'sample_AROME/ke_spec.npy')
-# n_sample, nk, nt = ke_spec.shape
-# dt = 1
-# for t in range(1, nt+1):
-#     wn = np.arange(20., 200.)*3./2.5
-#     pwr = np.mean(ke_spec[:, :, t], axis=0)/((3./2.5)**2)
-#     ax.loglog(wn, pwr[20:200], color=cmap[t*dt][0:3])
+sf = 5500./2375.
+ke_spec = np.load(pert_path+'sample_AROME/ke_spec.npy')
+n_sample, nk, nt = ke_spec.shape
+dt = 1
+for t in range(1, nt):
+    wn = np.arange(10., 200.)*sf
+    pwr = np.mean(ke_spec[:, :, t], axis=0)/sf/sf/2.
+    ax.loglog(wn, pwr[10:200], color=cmap[t*dt][0:3])
+
+##reference level
+wn = np.arange(1., 20.)
+pwr = np.mean(np.load(pert_path+'sample_ECMWF/ke_spec_ref.npy')[0, 1:20, 1:], axis=1)
+ax.loglog(wn, pwr, 'k')
+wn = np.arange(10., 200.)*sf
+pwr = np.mean(np.load(pert_path+'sample_AROME/ke_spec_ref.npy')[0, 10:200, 1:], axis=1)/sf/sf
+ax.loglog(wn, pwr, 'k')
 
 ##some ref lines
-wn = np.arange(5., 30.)
-ax.loglog(wn, 1e3*wn**(-3), 'r')
-wn = np.arange(20., 200.)
-ax.loglog(wn, 1e1*wn**(-5./3), 'r')
+wn = np.arange(5., 20.)
+ax.loglog(wn, 3e2*wn**(-3), 'r')
+wn = np.arange(30., 400.)
+ax.loglog(wn, 5*wn**(-5./3), 'r')
 
 ax.set_title('EKE')
-ax.set_xlabel('wavenumber')
+wl = np.array([1e4, 1e3, 1e2, 1e1])
+ax.set_xticks(5500./wl)
+ax.set_xticklabels(wl)
+ax.set_xlabel('wavelength (km)')
 
 plt.savefig(pert_path+'fig_spectrum.pdf')
 
--- perturbation/plot_correlation.py
+++ perturbation/plot_correlation.py
@@ -1,40 +1,39 @@
 import numpy as np
-from netCDF4 import Dataset
 import matplotlib.pyplot as plt
-from diag.metric import sample_correlation
-from diag.multiscale import pwrspec2d
+import grid.io.netcdf as nc
+from diag import sample_correlation
+import config.constants as cc
+import sys
 
 ##read synfoc_*nc and plot the diagnosed correlation length scales (time/space)
 
-vname = 'uwind'
-xdim = 900
-ydim = 900
-dx = 5
-dt = 6
+outdir = sys.argv[1]
+vname = sys.argv[2]
+n_sample = int(sys.argv[3])
+nt = int(sys.argv[4])
+dx = float(sys.argv[5])  ##km
+dt = float(sys.argv[6])  ##hour
+cp = cc.CYCLE_PERIOD/60.
 
-n_field = 50
-nens = 20
-
-outdir = '/cluster/work/users/yingyue/perturbation/'
-
-pert = np.zeros((nens, n_field, ydim, xdim))
-for n in range(n_field):
-    f = Dataset(outdir+'/synforc_{:04d}.nc'.format(n))
-    pert[:, n, :, :] = np.array(f[vname])[0, :, :, :]
+_, ny, nx = nc.read(outdir+'/{:03d}/{:04d}/perturb.nc'.format(1, int(dt/cp)), vname).shape
+pert = np.zeros((n_sample, nt, nx))
+for n in range(n_sample):
+    for m in range(1, nt):
+        pert[n, m-1, :] = nc.read(outdir+'/{:03d}/{:04d}/perturb.nc'.format(n+1, int(m*dt/cp)), vname)[0, int(0.5*ny), :]
 
 ##length in time/space to sample
-L = int(0.25*xdim)
+L = int(0.25*nx)
 hcorr = np.zeros(L)
 for l in range(L):
-    samp1 = pert[:, 0, :, int(0.25*xdim):int(0.75*xdim)].flatten()
-    samp2 = np.roll(pert[:, 0, :, int(0.25*xdim):int(0.75*xdim)], -l, axis=2).flatten()
+    samp1 = pert[:, :, int(0.25*nx):int(0.75*nx)].flatten()
+    samp2 = np.roll(pert, -l, axis=2)[:, :, int(0.25*nx):int(0.75*nx)].flatten()
     hcorr[l] = sample_correlation(samp1, samp2)
 
-T = int(0.5*n_field)
+T = int(0.5*nt)
 tcorr = np.zeros(T)
 for t in range(T):
-    samp1 = pert[:, 0:T, 0, :].flatten()
-    samp2 = np.roll(pert, -t, axis=1)[:, 0:T, 0, :].flatten()
+    samp1 = pert[:, 0:T, :].flatten()
+    samp2 = np.roll(pert, -t, axis=1)[:, 0:T, :].flatten()
     tcorr[t] = sample_correlation(samp1, samp2)
 
 plt.figure(figsize=(10,5))
@@ -48,5 +47,5 @@ ax.plot(np.arange(0, dt*T, dt), tcorr, 'k', linewidth=2)
 ax.set_title('temporal correlation')
 ax.set_xlabel('lag (h)')
 
-plt.savefig(outdir+'fig_correlation.pdf')
+plt.savefig(outdir+'/fig_correlation.pdf')
 
