83cbb4af8dd41a14f19471989079d225693d6101
Yue Ying <hardyying@gmail.com>
2023-01-10 13:13:59 +0100

    diff. way of sample fcst_err

--- perturbation/sample_forecast_error/process_AROME.py
+++ perturbation/sample_forecast_error/process_AROME.py
@@ -3,22 +3,22 @@ import grid.io.netcdf as nc
 import grid
 import config.constants as cc
 from pynextsim.irregular_grid_interpolator import IrregularGridInterpolator
-import datetime
+from datetime import datetime, timedelta
 import os
 import sys
 
 n_sample = int(sys.argv[1])
 
-t0 = datetime.datetime.strptime('202011010000', '%Y%m%d%H%M')
-dt_sample = datetime.timedelta(days=1.5)
-dt = datetime.timedelta(minutes=cc.CYCLE_PERIOD)
-cycle_period = datetime.timedelta(minutes=cc.CYCLE_PERIOD)
-Dt = datetime.timedelta(hours=66)
+t0 = datetime.strptime('201911010000', '%Y%m%d%H%M')
+dt_sample = timedelta(days=1.5)
+dt = timedelta(minutes=cc.CYCLE_PERIOD)
+cycle_period = timedelta(minutes=cc.CYCLE_PERIOD)
+Dt = timedelta(hours=66)
 varname = {'x_wind_10m':'x_wind_10m',
            'y_wind_10m':'y_wind_10m'}
            # 'atm_pressure':'air_pressure_at_sea_level'}
 
-dt_in_file = datetime.timedelta(hours=1)
+dt_in_file = timedelta(hours=1)
 assert(dt_in_file <= dt)
 
 def filename(t):
@@ -35,7 +35,7 @@ for fcst_step in range(1, int(Dt/dt)+1):
     t1 = t0 + n_sample*dt_sample
     file1 = filename(t1)
 
-    t2 = t1 - fcst_step*dt
+    t2 = t1 + fcst_step*dt
     file2 = filename(t2)
     t_index = int(fcst_step*dt/dt_in_file)
     print('sample ', n_sample, 'fcst_step=', int(fcst_step*dt/cycle_period), t1, t2, t_index)
@@ -44,16 +44,16 @@ for fcst_step in range(1, int(Dt/dt)+1):
     for v in varname:
         f1 = nc.Dataset(file1)
         f2 = nc.Dataset(file2)
-        dat_orig[v] = f2[varname[v]][t_index, 0, :, :] - f1[varname[v]][0, 0, :, :]
+        dat_orig[v] = f1[varname[v]][t_index, 0, :, :] - f2[varname[v]][0, 0, :, :]
         f1.close()
         f2.close()
 
-    out_path = cc.PERTURB_PARAM_DIR+'/sample_AROME/{:03d}/{:04d}'.format(n_sample, int(fcst_step*dt/cycle_period))
+    out_path = cc.PERTURB_PARAM_DIR+'/sample_AROME/{:03d}/'.format(n_sample)
     if not os.path.exists(out_path):
             os.makedirs(out_path)
 
     for v in varname:
         dat_grid = np.zeros((1, ny, nx))
         dat_grid[0, :, :] = dat_orig[v]
-        nc.write(out_path+'/perturb.nc', {'t':1, 'y':ny, 'x':nx}, v, dat_grid)
+        nc.write(out_path+'/perturb_{:04d}.nc'.format(int(fcst_step*dt/cycle_period)), {'t':1, 'y':ny, 'x':nx}, v, dat_grid)
 
--- perturbation/sample_forecast_error/process_ECMWF.py
+++ perturbation/sample_forecast_error/process_ECMWF.py
@@ -3,22 +3,22 @@ import grid.io.netcdf as nc
 import grid
 import config.constants as cc
 from pynextsim.irregular_grid_interpolator import IrregularGridInterpolator
-import datetime
+from datetime import datetime, timedelta
 import os
 import sys
 
 n_sample = int(sys.argv[1])
 
-t0 = datetime.datetime.strptime('201911010000', '%Y%m%d%H%M')
-dt_sample = datetime.timedelta(days=3)
-dt = datetime.timedelta(days=1)
-cycle_period = datetime.timedelta(minutes=cc.CYCLE_PERIOD)
-Dt = datetime.timedelta(days=10)
+t0 = datetime.strptime('201911010000', '%Y%m%d%H%M')
+dt_sample = timedelta(days=3)
+dt = timedelta(days=1)
+cycle_period = timedelta(minutes=cc.CYCLE_PERIOD)
+Dt = timedelta(days=10)
 varname = {'x_wind_10m':'10U',
            'y_wind_10m':'10V'}
            # 'atm_pressure':'MSL'}
 
-dt_in_file = datetime.timedelta(hours=6)
+dt_in_file = timedelta(hours=6)
 assert(dt_in_file <= dt)
 
 def filename(t):
@@ -31,13 +31,14 @@ lon, lat = np.meshgrid(nc.read(file0, 'lon'), nc.read(file0, 'lat'))
 tmp = grid.crs.transform_points(grid.cartopy.crs.PlateCarree(), lon, lat)
 x = tmp[:, :, 0]; y = tmp[:, :, 1]
 x_ref, y_ref = grid.make_uniform_grid(-2500000, 3000000, -2000000, 2500000, 20000)
+nx, ny = x_ref.shape
 igi = IrregularGridInterpolator(x, y, x_ref, y_ref)
 
 for fcst_step in range(1, int(Dt/dt)+1):
     t1 = t0 + n_sample*dt_sample
     file1 = filename(t1)
 
-    t2 = t1 - fcst_step*dt
+    t2 = t1 + fcst_step*dt
     file2 = filename(t2)
     t_index = int(fcst_step*dt/dt_in_file)
     print('sample ', n_sample, 'fcst_step=', int(fcst_step*dt/cycle_period), t1, t2, t_index)
@@ -46,7 +47,7 @@ for fcst_step in range(1, int(Dt/dt)+1):
     for v in varname:
         f1 = nc.Dataset(file1)
         f2 = nc.Dataset(file2)
-        dat_orig[v] = f2[varname[v]][t_index, :, :] - f1[varname[v]][0, :, :]
+        dat_orig[v] = f1[varname[v]][t_index, :, :] - f2[varname[v]][0, :, :]
         f1.close()
         f2.close()
 
@@ -54,12 +55,12 @@ for fcst_step in range(1, int(Dt/dt)+1):
     if 'x_wind_10m' in varname and 'y_wind_10m' in varname:
         dat['x_wind_10m'], dat['y_wind_10m'] = grid.rotate_vector(x, y, dat_orig['x_wind_10m'], dat_orig['y_wind_10m'])
 
-    out_path = cc.PERTURB_PARAM_DIR+'/sample_ECMWF/{:03d}/{:04d}'.format(n_sample, int(fcst_step*dt/cycle_period))
+    out_path = cc.PERTURB_PARAM_DIR+'/sample_ECMWF/{:03d}'.format(n_sample)
     if not os.path.exists(out_path):
             os.makedirs(out_path)
 
     for v in varname:
-        dat_grid = np.zeros((1, cc.NY, cc.NX))
+        dat_grid = np.zeros((1, ny, nx))
         dat_grid[0, :, :] = igi.interp_field(dat[v]).T
-        nc.write(out_path+'/perturb.nc', {'t':1, 'y':cc.NY, 'x':cc.NX}, v, dat_grid)
+        nc.write(out_path+'/perturb_{:04d}.nc'.format(int(fcst_step*dt/cycle_period)), {'t':1, 'y':ny, 'x':nx}, v, dat_grid)
 
