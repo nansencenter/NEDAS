8789c3b14707703f762ba44d6741c8ed2105d7c4
Yue Ying <hardyying@gmail.com>
2023-06-07 09:09:40 +0200

    faster fft, random field, spectral diag

--- diag/__init__.py
+++ diag/__init__.py
@@ -1,2 +1,41 @@
 ###util func for diagnostics
+import numpy as np
+from grid.multiscale import fft2, get_wn, get_scale_comp
+
+def rmse(fld, fld_tr):
+    return
+
+def pattern_corr(fld, fld_tr):
+    return
+
+##some spectral diagnostics
+def pwrspec2d(fld):
+    ##horizontal 2d power spectrum p(k2d)
+    ##input fld[..., ny, nx]
+    ##return wn[0:nup], pwr[..., 0:nup]
+    ##  nup is the max wavenumber given ny,nx
+    nx = fld.shape[-1]
+    ny = fld.shape[0]
+    kx, ky = get_wn(fld)
+    nupx = kx.max()
+    nupy = ky.max()
+    nup = int(max(nupx, nupy))
+
+    ##2d total wavenumber
+    k2d = np.sqrt((kx*(nup/nupx))**2 + (ky*(nup/nupy))**2)
+
+    ##2d fft of fld, convert to power (variance)
+    P = (np.abs(fft2(fld))/ny/nx)**2
+
+    ##sum all kx,ky points with same k2d
+    wn = np.arange(0., nup)
+    pwr = np.zeros(fld.shape[:-2] + (nup,))
+    for w in range(nup):
+        pwr[..., w] = np.sum(P[np.where(np.ceil(k2d)==w)])
+    return wn, pwr
+
+
+###some ensemble metrics
+def crps(fld_ens, fld_tr):
+    return
 
--- grid/multiscale.py
+++ grid/multiscale.py
@@ -2,32 +2,31 @@
 import numpy as np
 from numba import njit
 
-##A faster FFT solution from styfenschaer/rocket-fft
-from numba.np.numpy_support import as_dtype
-from rocket_fft import numpy_like
-from rocket_fft.overloads import _numpy_cmplx_lut
-numpy_like()
+###fft implementation using FFTW
+import pyfftw
 
-@njit
-def fftn(x, s=None, axes=None, norm=None):
-    return np.fft.fftn(x, s, axes, norm)
+def fft2(f):
+    ##prepare fftw plan
+    a = pyfftw.empty_aligned(f.shape, dtype='float32')
+    b = pyfftw.empty_aligned(f.shape[:-1] + (f.shape[-1]//2+1,), dtype='complex64')
+    fft_obj = pyfftw.FFTW(a, b, axes=(-2, -1))
+    ##perform the fft2, output fh with same shape as f (keep dimension info)
+    fh_ = fft_obj(f)
+    fh = np.zeros(f.shape, dtype='complex64')
+    fh[..., 0:f.shape[1]//2+1] = fh_
+    return fh
 
-@njit
-def ifftn(x, s=None, axes=None, norm=None):
-    return np.fft.ifftn(x, s, axes, norm)
+def ifft2(fh):
+    ##prepare fftw plan
+    b = pyfftw.empty_aligned(fh.shape[:-1] + (fh.shape[-1]//2+1,), dtype='complex64')
+    a = pyfftw.empty_aligned(fh.shape, dtype='float32')
+    fft_obj = pyfftw.FFTW(b, a, axes=(-2, -1), direction='FFTW_BACKWARD')
+    ##perform the ifft2
+    f = fft_obj(fh[..., 0:fh.shape[1]//2+1])
+    return f
 
-
-##convert between state and spectral spaces
-def grid2spec(f):
-    ni, nj = (f.shape[0], f.shape[1])
-    return np.fft.fft2(f, axes=(0, 1))
-
-def spec2grid(fk):
-    ni, nj = (fk.shape[0], fk.shape[1])
-    return np.real(np.fft.ifft2(fk, axes=(0, 1)))
-
-##generate wavenumber sequence for fft results
-def fft_wn(n):
+def fftwn(n):
+    ##wavenumber sequence for fft results in 1 dimension
     nup = int(np.ceil((n+1)/2))
     if n%2 == 0:
         wn = np.concatenate((np.arange(0, nup), np.arange(2-nup, 0)))
@@ -35,22 +34,29 @@ def fft_wn(n):
         wn = np.concatenate((np.arange(0, nup), np.arange(1-nup, 0)))
     return wn
 
-##generate meshgrid wavenumber for input field x
-## the first two dimensions are horizontal (i, j)
-def get_wn(x):
-    dims = x.shape
-    ii = fft_wn(dims[0])
-    jj = fft_wn(dims[1])
-    wni = np.expand_dims(ii, 1)
-    wni = np.repeat(wni, dims[1], axis=1)
-    wnj = np.expand_dims(jj, 0)
-    wnj = np.repeat(wnj, dims[0], axis=0)
-    for d in range(2, len(dims)):  ##extra dimensions
-        wni = np.expand_dims(wni, d)
-        wni = np.repeat(wni, dims[d], axis=d)
-        wnj = np.expand_dims(wnj, d)
-        wnj = np.repeat(wnj, dims[d], axis=d)
-    return wni, wnj
+def get_wn(fld):
+    ##generate meshgrid wavenumber for input field
+    ## the last two dimensions are horizontal (y, x)
+    ny, nx = fld.shape[-2:]
+    wnx = np.zeros(fld.shape)
+    wny = np.zeros(fld.shape)
+    for i in fftwn(fld.shape[-1]):
+        wnx[..., i] = i
+    for j in fftwn(fld.shape[-2]):
+        wny[..., j, :] = j
+    return wnx, wny
+
+def meshgrid(xi, yi):
+    nx = len(xi)
+    ny = len(yi)
+    x = np.full((ny, nx), np.nan)
+    y = np.full((ny, nx), np.nan)
+    for i in range(nx):
+        y[:, i] = yi
+    for i in range(ny):
+        x[i, :] = xi
+    return x, y
+
 
 ##scale decomposition
 def lowpass_resp(Kh, k1, k2):
@@ -73,7 +79,6 @@ def get_scale_resp(Kh, kr, s):
             resp = lowpass_resp(Kh, kr[s], kr[s+1]) - lowpass_resp(Kh, kr[s-1], kr[s])
     return resp
 
-
 def get_scale(x, kr, s):
     xk = grid2spec(x)
     xkout = xk.copy()
@@ -83,27 +88,4 @@ def get_scale(x, kr, s):
         Kh = np.sqrt(kx**2 + ky**2)
         xkout = xk * get_scale_resp(Kh, kr, s)
     return spec2grid(xkout)
-
-##power spectra
-def pwrspec2d(field):
-    '''
-    horizontal 2D spectrum p(k), k=sqrt(kx^2+ky^2)
-    input: field[ny, nx]
-    output: wn[nk], pwr[nk]
-    '''
-    ny, nx = field.shape
-    nupx = int(np.ceil((nx+1)/2))
-    nupy = int(np.ceil((ny+1)/2))
-    nup = max(nupx, nupy)
-    wnx = fft_wn(nx)
-    wny = fft_wn(ny)
-    kx, ky = np.meshgrid(wnx, wny)
-    k2d = np.sqrt((kx*(nup/nupx))**2 + (ky*(nup/nupy))**2)
-    FT = np.fft.fft2(field)
-    P = (np.abs(FT)/nx/ny)**2
-    wn = np.arange(0.0, nup)
-    pwr = np.zeros((nup,))
-    for w in range(nup):
-        pwr[w] = np.sum(P[np.where(np.ceil(k2d)==w)])
-    return wn, pwr
 
