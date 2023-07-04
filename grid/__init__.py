2dc2dff1bd95dd9ed00d6ed3fff65c7cebc9341f
Yue Ying <hardyying@gmail.com>
2023-07-04 12:09:40 +0200

    merged converter to the Grid class, and simplify

--- grid/__init__.py
+++ grid/__init__.py
@@ -1,13 +1,1 @@
 from .grid import Grid
-from .converter import Converter
-
-def regular_grid(xstart, xend, ystart, yend, dx, centered=False):
-    import numpy as np
-    xcoord = np.arange(xstart, xend, dx)
-    ycoord = np.arange(ystart, yend, dx)
-    x, y = np.meshgrid(xcoord, ycoord)
-    if centered:
-        x += 0.5*dx  ##move coords to center of grid box
-        y += 0.5*dx
-    return x, y
-
