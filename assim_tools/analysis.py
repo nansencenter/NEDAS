7b819d5b24a53cc36eca7b28691f83cf4dd66ddf
Yue Ying <hardyying@gmail.com>
2023-12-05 23:36:35 +0100

    comments

--- assim_tools/analysis.py
+++ assim_tools/analysis.py
@@ -4,12 +4,19 @@ from .parallel import distribute_tasks, message
 # from .state import loc_inds, read_field_info, uniq_fields, read_local_state, write_local_state
 # from .obs import read_obs_info, assign_obs_inds, read_local_obs
 
-##batch assimilation solves the matrix version EnKF analysis for a given local state variable,
+##batch assimilation solves the matrix version EnKF analysis for a given local state
 ##the local_analysis updates for different variables are computed in parallel
 @njit
-def local_analysis(ens_prior, obs, obs_err, obs_err_corr, obs_prior, loc_func, filter_kind='ETKF'):
-    ##update the local state variable ens_prior[nens] with obs[nlobs]
-    ##
+def local_analysis(ens_prior,          ##ensemble state [nens]
+                   obs,                ##obs [nlobs]
+                   obs_err,            ##obs err std [nlobs]
+                   obs_prior,          ##ensemble obs values [nens, nlobs]
+                   local_factor,       ##localization factor [nlobs]
+                   filter_kind='ETKF', ##kind of filter algorithm to apply
+                   obs_err_corr=None,  ##obs err corr matrix, if None, uncorrelated
+                   ):
+    ##update the local state variable ens_prior with the obs
+
     nens, nlobs = obs_prior.shape
     assert nens == ens_prior.size, 'ens_prior[{}] size mismatch with obs_prior[{},:]'.format(ens_prior.size, nens)
 
@@ -20,7 +27,7 @@ def local_analysis(ens_prior, obs, obs_err, obs_err_corr, obs_prior, loc_func, f
     ens_post = ens_prior.copy()
 
     ##ensemble weight matrix, weight[:, m] is for the m-th member
-    ##also known as T in Bishop 2001, and X5 in Evensen textbook (and in DEnKF Sakov 2012)
+    ##also known as T in Bishop 2001, and X5 in Evensen textbook (and in Sakov 2012)
     weight = np.zeros((nens, nens))
 
     ##find mean of obs_prior
@@ -29,20 +36,22 @@ def local_analysis(ens_prior, obs, obs_err, obs_err_corr, obs_prior, loc_func, f
         obs_prior_mean += obs_prior[m, :]
     obs_prior_mean /= nens
 
-    ##obs_prior_pert and innovation, normalized by sqrt(nens-1) R^-0.2, also known as S and dy in DEnKF
+    ##obs_prior_pert S and innovation dy, normalized by sqrt(nens-1) R^-0.2
     S = np.zeros((nlobs, nens))
     dy = np.zeros((nlobs))
     for p in range(nlobs):
-        S[p, :] = (obs_prior[:, p] - obs_prior_mean[p]) * loc_func[p] / obs_err / np.sqrt(nens-1)
-        dy[p] = (obs[p] - obs_prior_mean[p]) * loc_func[p] / obs_err / np.sqrt(nens-1)
+        S[p, :] = (obs_prior[:, p] - obs_prior_mean[p]) * local_factor[p] / obs_err
+        dy[p] = (obs[p] - obs_prior_mean[p]) * local_factor[p] / obs_err
 
     ##TODO:factor in the correlated R in obs_err, need another SVD of R
     # obs_err_corr
+    S /= np.sqrt(nens-1)
+    dy /= np.sqrt(nens-1)
 
     ##find singular values of the inverse variance ratio matrix (I + S^T S)
-    ##note: the added diagonal 1 here actually helps prevent issues if S^T S is not full rank
-    ##      when nlobs<nens, there will be singular values of 0, but the full matrix can still
-    ##      be inverted with singular values of 1.
+    ##note: the added I actually helps prevent issues if S^T S is not full rank
+    ##      when nlobs<nens, there will be singular values of 0, but the full matrix
+    ##      can still be inverted with singular values of 1.
     var_ratio_inv = np.eye(nens) + S.T @ S
     L, sv, Rh = np.linalg.svd(var_ratio_inv)
 
@@ -60,7 +69,16 @@ def local_analysis(ens_prior, obs, obs_err, obs_err_corr, obs_prior, loc_func, f
     var_ratio_sqrt = L @ np.diag(sv**-0.5) @ Rh
     weight += var_ratio_sqrt
 
-    ##now, transform the prior ensemble with the weight matrix
+    ##check if weights sum to 1
+    for m in range(nens):
+        sum_wgts = np.sum(weight[:, m])
+        if np.abs(sum_wgts - 1) > 1e-5:
+            raise ValueError('sum of weights is not 1')
+
+    ##finally, transform the prior ensemble with the weight matrix
+    ##in textbook, the weights are typically applied to the ens pert matrix
+    ##but since weights sum to 1, it is okay to just applied the weights
+    ##to the full ens_prior directly
     for m in range(nens):
         ens_post[m] = np.sum(ens_prior * weight[:, m])
 
@@ -71,7 +89,11 @@ def local_analysis(ens_prior, obs, obs_err, obs_err_corr, obs_prior, loc_func, f
 ##for each obs the near by state variables are updated one by one.
 ##so each update is a scalar problem, which is solved in 2 steps: obs_increment, update_ensemble
 @njit
-def obs_increment(obs_prior, obs, obs_err, filter_kind='EAKF'):
+def obs_increment(obs_prior,          ##obs prior [nens]
+                  obs,                ##obs
+                  obs_err,            ##obs err std
+                  filter_kind='EAKF', ##kind of filter algorithm
+                  ):
     ##compute analysis increment for 1 obs
     ##obs_prior[nens] is the prior ensemble values corresponding to this obs
     nens = obs_prior.size
@@ -86,6 +108,7 @@ def obs_increment(obs_prior, obs, obs_err, filter_kind='EAKF'):
     ##compute prior error variance
     obs_prior_var = np.sum(obs_prior_pert**2) / (nens-1)
 
+    ##ensemble adjustment Kalman filter (Anderson 2003)
     if filter_kind == 'EAKF':
         var_ratio = obs_var / (obs_prior_var + obs_var)
 
@@ -108,7 +131,12 @@ def obs_increment(obs_prior, obs, obs_err, filter_kind='EAKF'):
 
 
 @njit
-def update_ensemble(ens_prior, obs_prior, obs_incr, local_factor, reg_kind='linear'):
+def update_ensemble(ens_prior,
+                    obs_prior,
+                    obs_incr, 
+                    local_factor, 
+                    regress_kind='linear', 
+                    ):
     ##update the ensemble for 1 variable using the obs increments
     ##local_factor is the localization factor to reduce spurious correlation
     ##ens_prior[nens] is the ensemble values prior to update
