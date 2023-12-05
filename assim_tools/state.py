cadc6bd54e660a3c34ffe35d0fd097b695fdca34
Yue Ying <hardyying@gmail.com>
2023-12-05 23:36:35 +0100

    comments

--- assim_tools/state.py
+++ assim_tools/state.py
@@ -6,21 +6,30 @@ from datetime import datetime, timedelta
 from .common import type_convert, type_dic, type_size, t2h, h2t, t2s, s2t
 from .parallel import distribute_tasks, message
 
-## The entire state has dimensions: member, variable, time, and the 3D space z, y, x.
-## To organize the parallel tasks, we consider 3 indices: member, record, and location
-## mem_id (0, ..., nens-1) indexes the ensemble members
-## point_list is a list of point_id that indexes the spatial locations in a 2D field
-## field_list is a list of (mem_id, rec_id) that indexes the uniq 2D fields
+##Note: The analysis is performed on a regular grid.
+## The entire state has dimensions: member, variable, time,  z,  y,  x
+##                      indexed by: mem_id,        v,    t,  k,  j,  i
+##                       with size:   nens,       nv,   nt, nz, ny, nx
+##
+## To parallelize workload, we group the dimensions into 3 indices:
+## mem_id indexes the ensemble members
+## rec_id indexes the uniq 2D fields with (v, t, k), since nz and nt may vary
+##          for different variables, we stack these dimensions in the 'record'
+##          dimension with size nrec
+## loc_id indexes the location (j, i) in the 2D field.
+##
+## The entire state is distributed across the memory of many processors,
+## at any moment, a processor only stores a subset of state in its memory:
+## either having all the mem_id,rec_id but only a subset of loc_id (we call this
+## ensemble-complete), or having all the loc_id but a subset of mem_id,rec_id
+## (we call this field-complete).
+## It is easier to perform i/o and pre/post processing on field-complete state,
+## while easier to run assimilation algorithms with ensemble-complete state.
 
-##Note: only regular grid is supported for analysis now
 
-## This function parses info for the nens x nfld fields in the ensemble state.
-## returns: info dict with some dimensions and lists
-## -  info['fields'] points to records with uniq variable name, time and z (indexed by k),
-##    since nt and nz may vary for different variables, we stack them together in the rec dimension.
-## -  info['fld_list_proc'][pid] is a list of (mem_id, rec_id) for each pid
-## -  info['loc_list_proc'][pid] is a list of location slices (ist,ied,di,jst,jed,dj) for each pid
-
+## parses info for the nens * nrec fields in the ensemble state.
+## input: c: config module for parsing env variables
+## returns: info dict with some dimensions and list of uniq field records
 def parse_state_info(c):
     info = {'nx':c.nx, 'ny':c.ny, 'state_size':0, 'fields':{}}
     rec_id = 0   ##record id for a 2D field
@@ -148,17 +157,18 @@ def read_field(binfile, info, mask, mem_id, rec_id):
         return fld
 
 
-##routine to prepare the state variables, getting the fields from model restart files,
-##convert to the analysis grid, perform coarse-graining, and save to ens state in memory
+##routine to prepare the state variables, getting the fields from model restart
+##files, convert to the analysis grid, perform coarse-graining, save to ens state
 ##inputs: c: config module for parsing env variables
 ##        comm: mpi4py communicator for parallelization
 ##        state_info: info dict from parse_state_info
-##        fld_list_proc: list of field records to work on for each pid, each item
-##                       is (mem_id,rec_id) referring to a uniq field
-##        loc_list_proc: list of spatial locations to work on for each pid, each item
-##                       is (istart,iend,di,jstart,jend,dj) that describes a slice of the domain
-##returns: state dict with complete (mem_id, rec_id) pointing to the location slices in
-##         in loc_list_proc for each pid
+##        fld_list_proc: list of field records to work on for each pid,
+##                       each item is (mem_id,rec_id) referring to a uniq field
+##        loc_list_proc: list of spatial locations to work on for each pid,
+##                       each item is (istart,iend,di,jstart,jend,dj) that
+##                       describes a slice of the domain
+##returns: state dict with keys (mem_id, rec_id) pointing to the location
+##         slices in loc_list_proc for each pid
 def prepare_state(c, comm, state_info):
     pid = comm.Get_rank()
     nproc = comm.Get_size()
@@ -213,11 +223,13 @@ def prepare_state(c, comm, state_info):
             var = src.read_var(path, grid, member=mem_id, **rec)
             fld = grid.convert(var, is_vector=rec['is_vector'], method='linear', coarse_grain=True)
 
-        ##2. Perform communication to transpose the state from field-complete to ensemble-complete
-        ## For each source proc id (src_pid) with fld_list item (mem_id, rec_id), send chunk of
-        ## fld[..., jstart:jend:dj, istart:iend:di] to destination proc id (dst_pid) with its
-        ## corresponding slice in loc_list_proc
-        ## All pid needs to send/recv to/from all pid, so we use cyclic coreography here
+        ##2. transpose the state from field-complete to ensemble-complete
+        ## - for each source proc id (src_pid) with fld_list item (mem_id, rec_id),
+        ##   send chunk of fld[..., jstart:jend:dj, istart:iend:di] to destination
+        ##   proc id (dst_pid) with its corresponding slice in loc_list_proc
+        ## - every pid needs to send/recv to/from every pid, so we use cyclic
+        ##   coreography here to prevent deadlock
+
         ## 1) receive fld_chk from src_pid, for src_pid<pid first
         for src_pid in np.arange(0, pid):
             if task < len(fld_list_proc[src_pid]):
@@ -225,9 +237,9 @@ def prepare_state(c, comm, state_info):
                 state[src_mem_id, src_rec_id] = comm.recv(source=src_pid)
 
         ## 2) send my fld chunk to a list of dst_pid, send to dst_pid>=pid first
-        ##    because they are waiting to receive before being able to send stuff themselves;
-        ##    when finished with dst_pid>=pid, cycle back to send to dst_pid<pid, i.e.,
-        ##    dst_pid list is [pid, pid+1, ..., nproc-1, 0, 1, ..., pid-1]
+        ##    because they wait to receive before being able to send their own stuff;
+        ##    when finished with dst_pid>=pid, cycle back to send to dst_pid<pid,
+        ##    i.e., dst_pid list = [pid, pid+1, ..., nproc-1, 0, 1, ..., pid-1]
         if task < len(fld_list_proc[pid]):
             for dst_pid in np.mod(np.arange(0, nproc)+pid, nproc):
                 fld_chk = {}
