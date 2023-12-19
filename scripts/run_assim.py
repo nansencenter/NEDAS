41c989e125cb733884b68ec4369fa99cf7e03dd6
Yue Ying <hardyying@gmail.com>
2023-12-19 14:51:36 +0100

    don't put data in c module anymore
    As I almost always import config as c,
    I tried to put var in c.var so that I don't need to put them explicitly
    in function inputs. However, this is maybe not safe, depending on how
    python handles existing data in module load. So, I decided to do things
    the old-fashioned way, explicit input variable it is.

--- scripts/run_assim.py
+++ scripts/run_assim.py
@@ -20,53 +20,53 @@ else:
 ##1.Prepare state variables
 ##analysis grid in 2D is prepared based on config file
 ##c.grid obj has information on x,y coordinates
-timer1 = time.time()
+start = time.time()
 
 ##parse state info, for each member there will be a list of uniq fields
 message(c.comm, '1.Prepare state variables\n', 0)
 message(c.comm, 'ensemble size nens={}\n'.format(c.nens), 0)
 
-c.state_info = parse_state_info(c)
+state_info = parse_state_info(c)
 
-build_state_tasks(c)
+field_list = build_state_tasks(c, state_info)
 
 ##prepare the ensemble state and z coordinates
-fields, z_fields = prepare_state(c)
+fields, z_fields = prepare_state(c, state_info, field_list)
 
 ##save a copy of the prior state
 state_file = c.work_dir+'/analysis/'+c.time+c.s_dir+'/prior_state.bin'
-output_state(c, fields, state_file)
+output_state(c, state_info, field_list, fields, state_file)
 
 ##collect ensemble mean z fields as analysis grid z coordinates
 ##for obs preprocessing later
 message(c.comm, 'collect model z coordinates, ', 0)
 z_file = c.work_dir+'/analysis/'+c.time+c.s_dir+'/z_coords.bin'
-output_ens_mean(c, z_fields, z_file)
+output_ens_mean(c, state_info, field_list, z_fields, z_file)
 
-timer2 = time.time()
-message(c.comm, 'Step 1 took {} seconds\n\n'.format(timer2-timer1), 0)
+message(c.comm, 'Step 1 took {} seconds\n\n'.format(time.time()-start), 0)
+start = time.time()
 
 ##--------------------------
 ## 2.Prepare observations
 message(c.comm, '2.Prepare obs and obs priors\n', 0)
 
-c.obs_info = parse_obs_info(c)
+obs_info = parse_obs_info(c)
 
-build_obs_tasks(c)
+obs_list = build_obs_tasks(c, obs_info)
 
-obs_seq = prepare_obs(c)
+obs_seq = prepare_obs(c, state_info, obs_info, obs_list)
 
-c.loc_list_full = partition_grid(c)
+loc_list_full = partition_grid(c)
 
-c.obs_inds = assign_obs_to_loc(c, obs_seq)
+obs_inds = assign_obs_to_loc(c, loc_list_full, obs_info, obs_list, obs_seq)
 
-c.loc_list = build_loc_tasks(c)
+loc_list = build_loc_tasks(c, loc_list_full, obs_info, obs_inds)
 
 ##compute obs prior, each pid compute a subset of obs
-obs_prior_seq = prepare_obs_prior(c, obs_seq, fields, z_fields)
+obs_prior_seq = prepare_obs_prior(c, state_info, field_list, obs_info, obs_list, obs_seq, fields, z_fields)
 
-timer3 = time.time()
-message(c.comm, 'Step 2 took {} seconds\n\n'.format(timer3-timer2), 0)
+message(c.comm, 'Step 2 took {} seconds\n\n'.format(time.time()-start), 0)
+start = time.time()
 
 ##--------------------------
 ##3.Transposing fields to local ensemble-complete states:
@@ -74,22 +74,24 @@ message(c.comm, '3.Transpose from field-complete to ensemble-complete\n', 0)
 
 ##the local state variables to be updated
 message(c.comm, 'state variable fields: ', 0)
-state = transpose_field_to_state(c, fields)
+state = transpose_field_to_state(c, state_info, field_list, loc_list, fields)
 
 ##z_coords for state variables
 message(c.comm, 'z coords fields: ', 0)
-z_state = transpose_field_to_state(c, z_fields)
+z_state = transpose_field_to_state(c, state_info, field_list, loc_list, z_fields)
 
 ##global scalar state variables to be updated
 
 ##obs and obs_prior
 message(c.comm, 'obs sequences: ', 0)
-lobs = transpose_obs_to_lobs(c, obs_seq)
-message(c.comm, 'obs prior sequences: ', 0)
-lobs_prior = transpose_obs_to_lobs(c, obs_prior_seq, ensemble=True)
+lobs = transpose_obs_to_lobs(c, obs_list, obs_inds, loc_list, obs_seq)
 
-timer4 = time.time()
-message(c.comm, 'Step 3 took {} seconds\n\n'.format(timer4-timer3), 0)
+message(c.comm, 'obs prior sequences: ', 0)
+lobs_prior = transpose_obs_to_lobs(c, obs_list, obs_inds, loc_list, obs_prior_seq, ensemble=True)
+
+message(c.comm, 'Step 3 took {} seconds\n\n'.format(time.time()-start), 0)
+start = time.time()
+
 
 ##--------------------------
 ##4.Assimilate obs to update state variables
@@ -98,8 +100,8 @@ message(c.comm, '4.Assimilation\n', 0)
 if c.assim_mode == 'batch':
 
     ##loop through tiles stored on pid
-    for loc in range(len(c.loc_list[c.pid])):
-        ist,ied,di,jst,jed,dj = c.loc_list[c.pid][loc]
+    for loc in range(len(loc_list[c.pid])):
+        ist,ied,di,jst,jed,dj = loc_list[c.pid][loc]
         ii, jj = np.meshgrid(np.arange(ist,ied,di), np.arange(jst,jed,dj))
         mask_chk = c.mask[jst:jed:dj, ist:ied:di]
 
@@ -108,7 +110,7 @@ if c.assim_mode == 'batch':
         # obs_prior = []
         # obs_err = []
         # nlobs = 0
-        # for obs_rec_id, obs_rec in c.obs_info['records'].items():
+        # for obs_rec_id, obs_rec in obs_info['records'].items():
         #     if obs_rec['is_vector']:
         #         for v in range(2):
         #             obs.append(lobs[obs_rec_id][loc]['obs'][v, :])
@@ -124,7 +126,7 @@ if c.assim_mode == 'batch':
         ##loop through unmasked grid points in the tile
         for l in range(len(ii[~mask_chk])):
             ##loop through each field record
-            for rec_id, rec in c.state_info['fields'].items():
+            for rec_id, rec in state_info['fields'].items():
                 keys = [(0, l), (1, l)] if rec['is_vector'] else [l]
 
                 # for key in keys:
@@ -147,21 +149,21 @@ if c.assim_mode == 'batch':
 elif c.assim_mode == 'serial':
     pass
 
-timer5 = time.time()
-message(c.comm, 'Step 4 took {} seconds\n\n'.format(timer5-timer4), 0)
+message(c.comm, 'Step 4 took {} seconds\n\n'.format(time.time()-start), 0)
+start = time.time()
 
 ##--------------------------
 ##5.Transposing state back to field-complete
 message(c.comm, '5.Transpose state from ensemble-complete to field-complete\n', 0)
 
 message(c.comm, 'state variable fields: ', 0)
-fields = transpose_state_to_field(c, state)
+fields = transpose_state_to_field(c, state_info, field_list, loc_list, state)
 
 state_file = c.work_dir+'/analysis/'+c.time+c.s_dir+'/post_state.bin'
-output_state(c, fields, state_file)
+output_state(c, state_info, field_list, fields, state_file)
 
-timer6 = time.time()
-message(c.comm, 'Step 5 took {} seconds\n\n'.format(timer6-timer5), 0)
+message(c.comm, 'Step 5 took {} seconds\n\n'.format(time.time()-start), 0)
+start = time.time()
 
 ##--------------------------
 ##6.Post-processing
@@ -171,6 +173,6 @@ message(c.comm, '6.Post-processing\n', 0)
 ##else
 ## just copy output state to model restart files
 
-timer7 = time.time()
-message(c.comm, 'Step 6 took {} seconds\n\n'.format(timer7-timer6), 0)
+message(c.comm, 'Step 6 took {} seconds\n\n'.format(time.time()-start), 0)
+start = time.time()
 
