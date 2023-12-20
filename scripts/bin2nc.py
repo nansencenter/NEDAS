54ec0af1b97d69a593048d0376d0bb0d7e5bcb5d
Yue Ying <hardyying@gmail.com>
2023-12-20 14:31:38 +0100

    fix issue in bin2nc

--- scripts/bin2nc.py
+++ scripts/bin2nc.py
@@ -1,37 +1,29 @@
 import numpy as np
 import config as c
 from netcdf_lib import nc_write_var
-from assim_tools.common import t2h, h2t
-from assim_tools.state import parse_state_info, read_field
+from conversion import t2h, h2t
+from assim_tools.state import read_field_info, read_field
 
 import sys
 filename = sys.argv[1]
 
-##convert bin file state variables [nfield, ny, nx] * nens files
+##convert bin file state variables [nfield, ny, nx] * nens
 ##to nc files [nens, nt, nz, ny, nx] * num_var files
-info = parse_state_info(c)
-x = c.grid.x
-y = c.grid.y
-mask = c.mask
-flds = info['fields']
-ny = info['ny']
-nx = info['nx']
-nens = info['nens']
-dims = {'member':None, 'time':None, 'level':None, 'y':ny, 'x':nx}
+info = read_field_info(filename)
+dims = {'member':None, 'time':None, 'level':None, 'y':c.ny, 'x':c.nx}
 
 for v in list(set(rec['name'] for i, rec in info['fields'].items())):
     print('converting '+v)
     outfile = filename.replace('.bin','.'+v+'.nc')
 
-    members = np.arange(nens)
-    times = np.array(list(set(t2h(rec['time']) for i, rec in flds.items() if rec['name']==v)))
-    levels = np.array(list(set(rec['k'] for i, rec in flds.items() if rec['name']==v)))
-    ##sort of level k doesn't work: -10 -2 -3 ... -9 -1 -11...
+    members = np.arange(c.nens)
+    times = np.array(list(set(t2h(rec['time']) for i, rec in info['fields'].items() if rec['name']==v)))
+    levels = np.array(list(set(rec['k'] for i, rec in info['fields'].items() if rec['name']==v)))
 
-    for fld_id, rec in info['fields'].items():
-        for mem_id in range(nens):
+    for rec_id, rec in [(i,r) for i,r in info['fields'].items() if r['name']==v]:
+        for mem_id in range(c.nens):
             ##get the field from bin file
-            fld = read_field(filename, info, mask, mem_id, fld_id)
+            fld = read_field(filename, info, c.mask, mem_id, rec_id)
             ##get record number along time,level dimensions
             id_time = [i for i,t in enumerate(times) if t2h(rec['time'])==t][0]
             id_level = [i for i,z in enumerate(levels) if rec['k']==z][0]
