028efbaf8f950b69eb2da00d54ff28ee4fecf1d1
Yue Ying <hardyying@gmail.com>
2023-12-21 10:01:01 +0100

    adding vortex 2d model

---
+++ models/vort2d/__init__.py
@@ -0,0 +1,115 @@
+import numpy as np
+import glob
+from datetime import datetime
+from netcdf_lib import nc_read_var, nc_write_var
+
+from .model import initialize, advance_time
+from .param import *  ##model parameters are configured here
+
+##dictionary from state_def names to native variable names and properties
+##List of properties:
+##   name: native variable name in restart files, tuple of (u-name,v-name)
+##         if vector field components are stored in separate native variables
+##   dtype: double/flout/int
+##   is_vector: if true the variable contains (u, v) components
+##   restart_dt: how freq model output is available, in hours
+##   levels: vertical level index list
+##   units: native physical units for the variable
+variables = {'velocity': {'name':('u', 'v'), 'dtype':'float', 'is_vector':True, 'restart_dt':6, 'levels':np.array([0]), 'units':'m/s'}, }
+
+##parse kwargs and find matching filename
+##for keys in kwargs that are not set, here we define the default values
+##key values in kwargs will also be checked for erroneous values here
+def filename(path, **kwargs):
+    if 'time' in kwargs and kwargs['time'] is not None:
+        assert isinstance(kwargs['time'], datetime), 'time shall be a datetime object'
+        tstr = kwargs['time'].strftime('%Y%m%d_%H')
+    else:
+        tstr = '????????_??'
+    if 'member' in kwargs and kwargs['member'] is not None:
+        assert kwargs['member'] >= 0, 'member index shall be >= 0'
+        mstr = '_mem{:03d}'.format(kwargs['member'])
+    else:
+        mstr = ''
+
+    ##get a list of filenames with matching kwargs
+    search = path+'/'+tstr+mstr+'.nc'
+    flist = glob.glob(search)
+    assert len(flist)>0, 'no matching files found: '+search
+    ##typically there will be only one matching file given the kwargs,
+    ##if there is a list of matching files, then we return the first one
+    return flist[0]
+
+
+from pyproj import Proj
+from grid import Grid
+
+##topaz grid is fixed in time/space, so no keys needed
+uniq_grid_key = ()
+
+###path and kwargs here are dummy input since the grid is fixed
+def read_grid(path, **kwargs):
+    ##any map projection will work, just define one arbitrarily
+    proj = Proj('+proj=stere')
+    ##define the coordinates
+    ii, jj = np.meshgrid(np.arange(nx), np.arange(ny))
+    x = ii*dx
+    y = jj*dx
+    ##the domain is doubly periodic
+    grid = Grid(proj, x, y, cyclic_dim=('x','y'))
+    return grid
+
+
+def write_grid(path, **kwargs):
+    pass
+
+
+def read_mask(path, grid):
+    mask = np.full(grid.x.shape, False)  ##no grid points are masked
+    return mask
+
+
+##get the state variable with name in state_def
+##and other kwargs: time, level, and member to pinpoint where to get the variable
+##returns a 2D field defined on grid from read_grid
+def read_var(path, grid, **kwargs):
+    ##check name in kwargs and read the variables from file
+    assert 'name' in kwargs, 'please specify which variable to get, name=?'
+    name = kwargs['name']
+    assert name in variables, 'variable name '+name+' not listed in variables'
+    fname = filename(path, **kwargs)
+
+    if is_vector:
+        var1 = nc_read_var(fname, variables[name]['name'][0])
+        var2 = nc_read-var(fname, variables[name]['name'][1])
+        var = np.array([var1, var2])
+    else:
+        var = nc_read_var(fname, variables[name]['name'])
+
+    return var
+
+
+##output updated variable with name='varname' defined in state_def
+##to the corresponding model restart file
+def write_var(path, grid, var, **kwargs):
+    ##check name in kwargs
+    assert 'name' in kwargs, 'please specify which variable to write, name=?'
+    name = kwargs['name']
+    assert name in variables, 'variable name '+name+' not listed in variables'
+    fname = filename(path, **kwargs)
+
+    if kwargs['is_vector']:
+        for i in range(2):
+            nc_write_var(fname, ('y', 'x'), variables[name]['name'][i], var[i,...])
+    else:
+        nc_write_var(fname, ('y', 'x'), var, variables[name]['name'], var)
+
+
+##for z coordinates, nothing to do here other than return all zeros
+uniq_z_key = ()
+def z_coords(path, grid, **kwargs):
+    return np.zeros(grid.x.shape)
+
+
+
+
