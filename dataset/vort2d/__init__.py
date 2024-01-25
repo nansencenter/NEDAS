82f57bfe750ea7ad346eebf46b274b964f2d29ea
Yue Ying <hardyying@gmail.com>
2024-01-25 22:42:05 +0100

    add diagnostic vortex obs types
    not tested yet

--- dataset/vort2d/__init__.py
+++ dataset/vort2d/__init__.py
@@ -1,4 +1,5 @@
 import numpy as np
+import importlib
 from datetime import datetime, timedelta
 
 variables = {'velocity': {'dtype':'float', 'is_vector':True, 'z_units':'m', 'units':'m/s'},
@@ -8,21 +9,68 @@ variables = {'velocity': {'dtype':'float', 'is_vector':True, 'z_units':'m', 'uni
             }
 
 
-def random_network(path, grid, mask, z, **kwargs):
+def random_network(path, grid, mask, z, truth_path, **kwargs):
+
+    ##get truth vortex position, some network is vortex-following
+    i, j = get_vortex_position(truth_path, grid, **kwargs)
+    true_center_x, true_center_y = grid.x[j,i], grid.y[j,i]
+
+    if 'network_type' in kwargs:
+        network_type = kwargs['network_type']
+    else:
+        network_type = 'global'
+
     if kwargs['name'] == 'velocity':
-        nobs = 150  ##number of obs
-        # obs_range = 180000  ##observed range from vortex center, m
+
+        if network_type == 'global':
+            nobs = 1000  ##number of obs
+            y = np.random.uniform(grid.ymin, grid.ymax, nobs)
+            x = np.random.uniform(grid.xmin, grid.xmax, nobs)
+
+        elif network_type == 'targeted':
+            nobs = 800  ##note: number of obs in entire domain
+                        ##later only obs within range will be kept
+            obs_range = 180000  ##observed range from vortex center, m
+            y = np.random.uniform(grid.ymin, grid.ymax, nobs)
+            x = np.random.uniform(grid.xmin, grid.xmax, nobs)
+
+            dist = np.hypot(x - true_center_x, y - true_center_y)
+            ind = np.where(dist <= obs_range)
+            x = x[ind]
+            y = y[ind]
+            nobs = x.size
+
+        else:
+            raise ValueError('unknown network type: '+network_type)
 
         obs_seq = {'obs': np.full(nobs, np.nan),
                    't': np.full(nobs, kwargs['time']),
                    'z': np.zeros(nobs),
-                   'y': np.random.uniform(grid.ymin, grid.ymax, nobs),
-                   'x': np.random.uniform(grid.xmin, grid.xmax, nobs),
+                   'y': y,
+                   'x': x,
                    'err_std': np.ones(nobs) * kwargs['err']['std']
                   }
 
-    ##TODO: diagnostic vortex obs
-    ##get truth velocity field
+    elif kwargs['name'] == 'vortex_position':
+        obs_seq = {'obs': np.array([[np.nan, np.nan]]),
+                   't': np.array([kwargs['time']]),
+                   'z': np.array([0]),
+                   'y': np.array([true_center_y]),
+                   'x': np.array([true_center_x]),
+                   'err_std': np.array([kwargs['err']['std']])
+                   }
+
+    elif kwargs['name'] in ['vortex_intensity', 'vortex_size']:
+        obs_seq = {'obs': np.array([np.nan]),
+                   't': np.array([kwargs['time']]),
+                   'z': np.array([0]),
+                   'y': np.array([true_center_y]),
+                   'x': np.array([true_center_x]),
+                   'err_std': np.array([kwargs['err']['std']])
+                   }
+
+    else:
+        raise ValueError('unknown obs variable: '+kwargs['name'])
 
     return obs_seq
 
@@ -30,33 +78,72 @@ def random_network(path, grid, mask, z, **kwargs):
 ##some observation operators obs_operator[model][obs_var_name]
 obs_operator = {}
 
+def get_velocity(path, grid, **kwargs):
+    model = importlib.import_module('models.vort2d')
+
+    ##get the velocity field from model
+    kwargs['name'] = 'velocity'
+    true_velocity = model.read_var(path, grid, **kwargs)
+
+    return true_velocity[0, ...], true_velocity[1, ...]
+
+
 def get_vortex_position(path, grid, mask, z, **kwargs):
-    
-    u, v = (X[:, :, 0], X[:, :, 1])
-    ni, nj = u.shape
-    zeta = (np.roll(v, -1, axis=0) - np.roll(v, 1, axis=0) - np.roll(u, -1, axis=1) + np.roll(u, 1, axis=1))/2.0
+    u, v = get_velocity(path, grid, **kwargs)
+    ny, nx = u.shape
+
+    ##compute vorticity
+    zeta = (np.roll(v, -1, axis=1) - np.roll(v, 1, axis=1) - np.roll(u, -1, axis=0) + np.roll(u, 1, axis=0)) / 2.0
+
+    ##search for max vorticity
     zmax = -999
-    ic, jc = (-1, -1)
-    ##coarse search
+    center_x, center_y = -1, -1
     buff = 6
-    for i in range(buff, ni-buff):
-        for j in range(buff, nj-buff):
-            z = np.sum(zeta[i-buff:i+buff, j-buff:j+buff])
+    for j in range(buff, ny-buff):
+        for i in range(buff, nx-buff):
+            z = np.sum(zeta[j-buff:j+buff, i-buff:i+buff])
             if z > zmax:
                 zmax = z
-                ic, jc = (i, j)
+                center_i, center_j = i, j
 
-    return (center_x, center_y)
+    return center_i, center_j
 
 
-def get_vortex_intensity():
+def get_vortex_intensity(path, grid, mask, z **kwargs):
+    u, v = get_velocity(path, grid, **kwargs)
+    Vmax = np.max(np.hypot(u, v))
 
     return Vmax
 
 
-def get_vortex_size():
+def get_vortex_size(path, grid, mask, z, **kwargs):
+    u, v = get_velocity(path, grid, **kwargs)
+    wind = np.hypot(u, v)
+    center_i, center_j = get_vortex_position(path, grid, mask, z, **kwargs)
+    ny, nx = wind.shape
 
-    return Rsize
+    nr = 30
+    wind_min = 15
+    wind_rad = np.zeros(nr)
+    count_rad = np.zeros(nr)
+    for j in range(-nr, nr+1):
+        for i in range(-nr, nr+1):
+            r = int(np.sqrt(i**2+j**2))
+            if r < nr:
+                wind_rad[r] += wind[int(center_j+j)%ny, int(center_i+i)%nx]
+                count_rad[r] += 1
+    wind_rad = wind_rad/count_rad
+
+    if np.max(wind_rad)<wind_min or np.where(wind_rad>=wind_min)[0].size==0:
+        Rsize = -1
+    else:
+        i1 = np.where(wind_rad>=wind_min)[0][-1] ###last point with wind > 35knot
+        if i1==nr-1:
+            Rsize = i1
+        else:
+            Rsize = i1 + (wind_rad[i1] - wind_min) / (wind_rad[i1] - wind_rad[i1+1])
+
+    return Rsize * grid.dx
 
 
 obs_operator['vort2d'] = {'vortex_position': get_vortex_position,
