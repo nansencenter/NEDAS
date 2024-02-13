05e42f1fd5dd0521eaa499474409ec9560baa847
Yue Ying <hardyying@gmail.com>
2024-02-13 16:08:16 +0100

    more options for qg random_network

--- dataset/qg/__init__.py
+++ dataset/qg/__init__.py
@@ -10,13 +10,23 @@ variables = {'velocity': {'dtype':'float', 'is_vector':True, 'z_units':'*', 'uni
 
 def random_network(path, grid, mask, z, truth_path, **kwargs):
 
-    nobs = 5000  ##number of obs
+    nobs = 1000  ##number of obs
     y = np.random.uniform(grid.ymin, grid.ymax, nobs)
     x = np.random.uniform(grid.xmin, grid.xmax, nobs)
 
+    # obs_thin = 1
+    # x, y = np.meshgrid(np.arange(grid.xmin, grid.xmax, obs_thin),
+    #                    np.arange(grid.ymin, grid.ymax, obs_thin))
+    # x = x.flatten()
+    # y = y.flatten()
+    # nobs = x.size
+
+    # z = np.random.uniform(1, 7, nobs)
+    z = np.zeros(nobs)
+
     obs_seq = {'obs': np.full(nobs, np.nan),
                't': np.full(nobs, kwargs['time']),
-               'z': np.zeros(nobs),
+               'z': z,
                'y': y,
                'x': x,
                'err_std': np.ones(nobs) * kwargs['err']['std']
