60d951157ee2e5ecd1479e8e16d9dfaa2871f403
Yue Ying <hardyying@gmail.com>
2024-03-18 16:43:21 +0100

    update_restart routine is adjusted

--- assim_tools/update.py
+++ assim_tools/update.py
@@ -18,6 +18,11 @@ def update_restart(c, state_info, mem_list, rec_list, fields_prior, fields_post)
     - field_prior, field_post
       the field-complete state variables before and after assimilate()
     """
+
+    pid_mem_show = [p for p,lst in mem_list.items() if len(lst)>0][0]
+    pid_rec_show = [p for p,lst in rec_list.items() if len(lst)>0][0]
+    c.pid_show = pid_rec_show * c.nproc_mem + pid_mem_show
+
     message(c.comm, 'update model restart files with analysis increments\n', c.pid_show)
 
     grid_bank = {}
@@ -59,14 +64,33 @@ def update_restart(c, state_info, mem_list, rec_list, fields_prior, fields_post)
 
             ##misc. inverse transform
 
-            ##output the posterior variable back to restart files
-            var_post = c.grid.convert(fld_post, is_vector=rec['is_vector'], method='linear')
+            ##convert the posterior variable back to native model grid
+            var_prior = src.read_var(path, grid, member=mem_id, **rec)
+            if rec['is_vector']:
+                var_post = c.grid.convert(fld_post, is_vector=rec['is_vector'], method='linear')
+            else:
+                var_post = c.grid.interp(fld_post, grid.x_elem, grid.y_elem)
+                # var_post_coarse = c.grid.convert(fld_post, method='linear', coarse_grain=True)
+                # var_post_coarse = np.nanmean(var_post_coarse[grid.tri.triangles], axis=1)
+
+            ##TODO: temporary solution for nan values due to interpolation
+            ind = np.where(np.isnan(var_post))
+            var_post[ind] = var_prior[ind]
+
+            ##post-processing by model module
+            if hasattr(src, 'postproc'):
+                var_post = src.postproc(var_post, **rec)
+
+            if np.isnan(var_post).any():
+                raise ValueError('nan detected in var_post')
+
+            ##write the posterior variable to restart file
             src.write_var(path, grid, var_post, member=mem_id, **rec)
 
     message(c.comm, ' done.\n', c.pid_show)
 
     ##clean up
-    del grid_bank, grid, var_post, fld_post
+    # del grid_bank, grid, var_post, fld_post
 
 
 ##alignment technique
