a5f7011494a22e310da35e5b5f8ff42a615fd0fe
Yue Ying <hardyying@gmail.com>
2024-05-31 20:17:25 +0200

    add dump_yaml to config class

--- config/config.py
+++ config/config.py
@@ -1,56 +1,57 @@
 import numpy as np
 import os
 import inspect
+import yaml
 import importlib
 import pyproj
-
-from .parse_config import parse_config
 from grid import Grid
 from utils.parallel import Comm
-from utils.conversion import s2t
-from utils.log import message
+from utils.conversion import s2t, t2s
+from .parse_config import parse_config
 
 class Config(object):
 
     def __init__(self, config_file=None, parse_args=False, **kwargs):
-
         ##parse config file and obtain a list of attributes
         code_dir = os.path.dirname(inspect.getfile(self.__class__))
         config_dict = parse_config(code_dir, config_file, parse_args, **kwargs)
+
+        self.keys = []
         for key, value in config_dict.items():
             setattr(self, key, value)
+            self.keys.append(key)
 
-        ##create work_dir
-        if not os.path.exists(self.work_dir):
-            os.makedirs(self.work_dir)
+        self.set_time()
+        self.set_comm()
+        self.set_analysis_grid()
+        self.set_model_config()
 
-        ##convert time string to datetime object
-        self.time_start = s2t(self.time_start)
-        self.time_end = s2t(self.time_end)
-        self.time_assim_start = s2t(self.time_assim_start)
-        self.time_assim_end = s2t(self.time_assim_end)
-        if hasattr(self, 'time'):
-            self.time = s2t(self.time)
-        else:
-            self.time = self.time_start
-        self.prev_time = self.time_start
-        self.next_time = self.time_start
 
+    def set_time(self):
+        for key in ['time_start', 'time_end', 'time_assim_start', 'time_assim_end']:
+            assert key in self.keys, f"'{key}' is missing in config file"
+        for key in ['time', 'prev_time', 'next_time']:
+            if key not in self.keys:
+                setattr(self, key, self.time_start)
+                self.keys.append(key)
+
+        ##convert string to datetime obj
+        for key in ['time_start', 'time_end', 'time_assim_start', 'time_assim_end', 'time', 'prev_time', 'next_time']:
+            setattr(self, key, s2t(getattr(self, key)))
+
+
+    def set_comm(self):
         ##initialize mpi communicator
         self.comm = Comm()
-
         if not hasattr(self, 'nproc'):
             self.nproc = self.comm.Get_size()
-
         self.pid = self.comm.Get_rank()  ##current processor id
 
         ##divide processors into mem/rec groups
         if not hasattr(self, 'nproc_mem'):
             self.nproc_mem = self.nproc
         assert self.nproc % self.nproc_mem == 0, "nproc {self.nproc} is not evenly divided by nproc_mem {self.nproc_mem}"
-
         self.nproc_rec = int(self.nproc/self.nproc_mem)
-
         self.pid_mem = self.pid % self.nproc_mem
         self.pid_rec = self.pid // self.nproc_mem
         self.comm_mem = self.comm.Split(self.pid_rec, self.pid_mem)
@@ -58,6 +59,8 @@ class Config(object):
 
         self.pid_show = 0  ##which pid is showing progress messages, default to root=0
 
+
+    def set_analysis_grid(self):
         ##initialize analysis grid
         if self.grid_def['type'] == 'custom':
             proj = pyproj.Proj(self.grid_def['proj'])
@@ -78,13 +81,36 @@ class Config(object):
         # if self.mask
         self.mask = np.full((self.grid.ny, self.grid.nx), False, dtype=bool)
 
+
+    def set_model_config(self):
         ##initialize model config
         self.model_config = {}
         for model_name, kwargs in self.model_def.items():
             module = importlib.import_module('models.'+model_name)
             self.model_config[model_name] = getattr(module, 'Model')(**kwargs)
 
-        ##print a summary
-        message(self.comm, f"Initializing config...\n\n working directory: {self.work_dir}\n\n parallel scheme: nproc = {self.nproc}, nproc_mem = {self.nproc_mem}\n\n analysis grid: type = {self.grid_def['type']}, shape = {self.grid.x.shape}\n\n cycling from {self.time_start} to {self.time_end}\n assimilation start at {self.time_assim_start}, cycle_period = {self.cycle_period} hours\n\n", self.pid_show)
 
+    def show_summary(self):
+        ##print a summary
+        print(f"""Initializing config...
+ working directory: {self.work_dir}
+ parallel scheme: nproc = {self.nproc}, nproc_mem = {self.nproc_mem}
+ cycling from {self.time_start} to {self.time_end}
+ assimilation start at {self.time_assim_start}
+ cycle_period = {self.cycle_period} hours
+ current time: {self.time}
+ """, flush=True)
+
+
+    def dump_yaml(self, config_file):
+        print(f"saving configuration to {config_file}", flush=True)
+
+        config_dict = {}
+        with open(config_file, 'w') as f:
+            for key in self.keys:
+                value = getattr(self, key)
+                if key in ['time_start', 'time_end', 'time_assim_start', 'time_assim_end', 'time', 'prev_time', 'next_time']:
+                    value = t2s(value)
+                config_dict[key] = value
+            yaml.dump(config_dict, f)
 
