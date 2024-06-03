3f2ad47aba39c9414410616f16debcb680f70dab
Yue Ying <hardyying@gmail.com>
2024-06-03 16:01:13 +0200

    fix typo: assimilate.py
    and some other minor issues:
    job_submit.sh: the nnode calculation is off by 1
    subprocess.run error handing added
    timer for transpose doesn't work

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -3,12 +3,12 @@
 #SBATCH --job-name=run_cycle
 #SBATCH --time=0-01:00:00
 #SBATCH --nodes=4
-#SBATCH --ntasks-per-node=128
+#SBATCH --ntasks-per-node=50
 #SBATCH --partition=normal
 #SBATCH --output=log/%j.out
 
 source $HOME/.bashrc
 source $HOME/code/NEDAS/config/env/betzy/python.src
 
-python $HOME/code/NEDAS/scripts/run_exp.py --config_file=$NEDAS/config/default.yml --nproc $SLURM_NTASKS --nproc_mem $SLURM_NTASKS --nens 500 --work_dir $SCRATCH/qg2
+python $HOME/code/NEDAS/scripts/run_exp.py --config_file=$NEDAS/config/samples/qg.yml --nproc $SLURM_NTASKS --nproc_mem $SLURM_NTASKS --nens 100 --work_dir $SCRATCH/qg2
 
