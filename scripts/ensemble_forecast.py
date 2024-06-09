5f11533972dab99a2b74dd038b013528f8b62563
Yue Ying <hardyying@gmail.com>
2024-06-09 22:05:39 +0200

    two options in ensemble_forecast

--- scripts/ensemble_forecast.py
+++ scripts/ensemble_forecast.py
@@ -3,58 +3,63 @@ import os
 from utils.conversion import t2s
 from utils.parallel import Scheduler
 
-def ensemble_forecast(c):
+def ensemble_forecast_scheduler(c, model_name):
     """
     This function runs ensemble forecasts to advance to the next cycle
     """
-    for model_name, model in c.model_config.items():
-        print(f"start {model_name} ensemble forecast", flush=True)
+    model = c.model_config[model_name]
+    print(f"start {model_name} ensemble forecast", flush=True)
 
-        nproc_per_job = c.model_def[model_name].get('nproc_per_mem', 1)
-        walltime = c.model_def[model_name].get('walltime', 10000)
-        nworker = c.nproc // nproc_per_job
-        scheduler = Scheduler(nworker, walltime)
+    nproc_per_job = c.model_def[model_name].get('nproc_per_mem', 1)
+    walltime = c.model_def[model_name].get('walltime', 10000)
+    nworker = c.nproc // nproc_per_job
+    scheduler = Scheduler(nworker, walltime)
 
-        path = os.path.join(c.work_dir, 'cycle', t2s(c.time), model_name)
-        if not os.path.exists(path):
-            os.makedirs(path)
+    path = os.path.join(c.work_dir, 'cycle', t2s(c.time), model_name)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-        for mem_id in range(c.nens):
+    for mem_id in range(c.nens):
 
-            job_name = model_name+f'_mem{mem_id+1}'
-            job = model
-
-            path = os.path.join(c.work_dir, 'cycle', t2s(c.time), model_name)
-
-            job_opt = {'host': c.host,
-                       'nedas_dir': c.nedas_dir,
-                       'path': path,
-                       'member': mem_id,
-                       'time': c.time,
-                       }
-            # print(job_opt)
-
-            scheduler.submit_job(job_name, job, **job_opt)  ##add job to the queue
-
-        scheduler.start_queue() ##start the job queue
-
-        if scheduler.error_jobs:
-            raise RuntimeError(f'scheduler: there are jobs with errors: {scheduler.error_jobs}')
-
-        print(' done.', flush=True)
-
-
-def ensemble_forecast_batch(c):
-    for model_name, model in c.model_config.items():
-        print(f"start {model_name} ensemble forecast ...", flush=True)
+        job_name = model_name+f'_mem{mem_id+1}'
+        job = model
 
         path = os.path.join(c.work_dir, 'cycle', t2s(c.time), model_name)
-        if not os.path.exists(path):
-            os.makedirs(path)
 
-        model.run(nens=c.nens, path=path, time=c.time)
+        job_opt = {'host': c.host,
+                    'nedas_dir': c.nedas_dir,
+                    'path': path,
+                    'member': mem_id,
+                    'time': c.time,
+                    }
+        # print(job_opt)
 
-        print('done.', flush=True)
+        scheduler.submit_job(job_name, job, **job_opt)  ##add job to the queue
+
+    scheduler.start_queue() ##start the job queue
+
+    if scheduler.error_jobs:
+        raise RuntimeError(f'scheduler: there are jobs with errors: {scheduler.error_jobs}')
+
+    print(' done.', flush=True)
+
+
+def ensemble_forecast_batch(c, model_name):
+    """
+    This functions runs ensemble forecast in one processor
+    Requires the model.run to propagate an ensemble state forward in time
+    in a simultaneous manner
+    """
+    model = c.model_config[model_name]
+    print(f"start {model_name} ensemble forecast ...", flush=True)
+
+    path = os.path.join(c.work_dir, 'cycle', t2s(c.time), model_name)
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    model.run(nens=c.nens, path=path, time=c.time)
+
+    print('done.', flush=True)
 
 
 if __name__ == "__main__":
@@ -64,5 +69,11 @@ if __name__ == "__main__":
 
     c = Config(parse_args=True)  ##get config from runtime args
 
-    timer(c)(ensemble_forecast)(c)
+    for model_name, model in c.model_config.items():
+        ens_run_type = c.model_def[model_name]['ens_run_type']
+        if ens_run_type == 'batch':
+            timer(c)(ensemble_forecast_batch)(c, model_name)
+        elif ens_run_type == 'scheduler':
+            timer(c)(ensemble_forecast_scheduler)(c, model_name)
+
 
--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #SBATCH --account=nn2993k
-#SBATCH --job-name=run_cycle
-#SBATCH --time=0-01:00:00
+#SBATCH --job-name=qg.emulator.n1000
+#SBATCH --time=0-04:00:00
 #SBATCH --nodes=4
-#SBATCH --ntasks-per-node=50
+#SBATCH --ntasks-per-node=128
 #SBATCH --partition=normal
-#SBATCH --output=log/%j.out
+#SBATCH --output=log/%x.out
+nens=1000
+model=qg.emulator
 
 source $HOME/.bashrc
 source $HOME/code/NEDAS/config/env/betzy/python.src
 
-python $HOME/code/NEDAS/scripts/run_exp.py --config_file=$NEDAS/config/samples/qg.yml --nproc $SLURM_NTASKS --nproc_mem $SLURM_NTASKS --nens 60 --work_dir $SCRATCH/qg2
+python $HOME/code/NEDAS/scripts/run_exp.py --config_file=$NEDAS/config/samples/$model.yml --nproc $SLURM_NTASKS --nproc_mem $SLURM_NTASKS --nens $nens --work_dir $SCRATCH/$model.n$nens
 
