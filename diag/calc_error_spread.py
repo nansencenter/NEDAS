df4cc32dcf289ba4df7687864c8db649d44cf7be
Yue Ying <hardyying@gmail.com>
2024-06-10 23:39:41 +0200

    diag: calc err and sprd for each cycle

---
+++ diag/calc_error_spread.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+from config import Config
+from utils.conversion import t2s, s2t, dt1h
+
+def get_scale(fld, krange, s):
+    return fld
+sc = ''
+
+krange = (1, 5)
+s = 0
+# from utils.multiscale import get_scale
+# sc = f"_s{s}"
+
+nedas_dir = '/cluster/home/yingyue/code/NEDAS'
+model_name = sys.argv[1] ##'qg'
+c = Config(config_file=nedas_dir+"/config/samples/"+model_name+".yml")
+c.nens = int(sys.argv[2]) ##20
+c.work_dir = "/cluster/work/users/yingyue/"+model_name+f".n{c.nens}"
+
+model = c.model_config[model_name]
+c.time_start = s2t('202301010000')
+c.time_end = s2t('20230218000')
+
+varname='streamfunc'
+vmin = -6
+vmax = 6
+is_vector = False
+
+def calc_error_spread(t, prev_t):
+    ##truth
+    path = c.work_dir+'/truth/'+t2s(t)+'/'+model_name
+    fld = model.read_var(path=path, name=varname, is_vector=is_vector, time=t)
+    truth = get_scale(fld, krange, s)
+
+    ##ens-mean prior
+    path = c.work_dir+'/cycle/'+t2s(prev_t)+'/'+model_name
+    ens_state_prior = np.full((c.nens,model.nz,model.ny,model.nx), np.nan)
+    for m in range(c.nens):
+        if not os.path.exists(model.filename(path=path, time=t, member=m)):
+            continue
+        fld = model.read_var(path=path, name=varname, is_vector=is_vector, time=t, member=m)
+        ens_state_prior[m, ...] = get_scale(fld, krange, s)
+    prior_mean = np.mean(ens_state_prior, axis=0)
+    prior_var = np.sum((ens_state_prior-prior_mean)**2, axis=0) / (c.nens-1)
+
+    ##ens-mean posterior
+    path = c.work_dir+'/cycle/'+t2s(t)+'/'+model_name
+    ens_state_post = np.full((c.nens,model.nz,model.ny,model.nx), np.nan)
+    for m in range(c.nens):
+        if not os.path.exists(model.filename(path=path, time=t, member=m)):
+            continue
+        fld = model.read_var(path=path, name=varname, is_vector=is_vector, time=t, member=m)
+        ens_state_post[m, ...] = get_scale(fld, krange, s)
+    post_mean = np.mean(ens_state_post, axis=0)
+    post_var = np.sum((ens_state_post-post_mean)**2, axis=0) / (c.nens-1)
+
+    rmse_prior = (np.sqrt(np.mean((prior_mean - truth)**2)))
+    sprd_prior = (np.sqrt(np.mean(prior_var)))
+    rmse_post = (np.sqrt(np.mean((post_mean - truth)**2)))
+    sprd_post = (np.sqrt(np.mean(post_var)))
+
+    path = c.work_dir+'/cycle/'+t2s(t)+'/'+model_name
+    print(path)
+    np.save(path+"/rmse_prior"+sc+".npy", rmse_prior)
+    np.save(path+"/rmse_post"+sc+".npy", rmse_post)
+    np.save(path+"/sprd_prior"+sc+".npy", sprd_prior)
+    np.save(path+"/sprd_post"+sc+".npy", sprd_post)
+
+
+t0 = c.time_start
+t = t0
+prev_t = t
+
+with ProcessPoolExecutor(max_workers=40) as exe:
+    futures = []
+    while t < c.time_end:
+        next_t = t + dt1h * c.cycle_period
+        future = exe.submit(calc_error_spread, t, prev_t)
+        futures.append(future)
+        prev_t = t
+        t = next_t
+    for future in as_completed(futures):
+        try:
+            result = future.result()
+        except Exception as e:
+            print(f'error occurred: {e}')
+
+
--- utils/multiscale.py
+++ utils/multiscale.py
@@ -1,81 +1,118 @@
 import numpy as np
 
-from .fft_lib import fft2, ifft2, fftwn, get_wn
+# from .fft_lib import fft2, ifft2, fftwn, get_wn
+from models.qg.util import spec2grid, grid2spec
 
 ##scale decomposition
-def lowpass_resp(k2d, k1, k2):
-    r = np.zeros(k2d.shape)
-    r[np.where(k2d<k1)] = 1
-    r[np.where(k2d>k2)] = 0
+# def lowpass_resp(k2d, k1, k2):
+#     r = np.zeros(k2d.shape)
+#     r[np.where(k2d<k1)] = 1
+#     r[np.where(k2d>k2)] = 0
 
-    ind = np.where(np.logical_and(k2d>=k1, k2d<=k2))
+#     ind = np.where(np.logical_and(k2d>=k1, k2d<=k2))
 
-    ##linear
-    # r[ind] = (np.log(k2) - np.log(k2d[ind]))/(np.log(k2) - np.log(k1))
-    r[ind] = (k2 - k2d[ind])/(k2 - k1)
-    ##cos-square
-    # r[ind] = np.cos((k2d[ind] - k1)*(0.5*np.pi/(k2 - k1)))**2
+#     ##linear
+#     # r[ind] = (np.log(k2) - np.log(k2d[ind]))/(np.log(k2) - np.log(k1))
+#     r[ind] = (k2 - k2d[ind])/(k2 - k1)
+#     ##cos-square
+#     # r[ind] = np.cos((k2d[ind] - k1)*(0.5*np.pi/(k2 - k1)))**2
 
+#     return r
+
+
+# def scale_response(k2d, scales, s):
+#     ns = len(scales)
+#     resp = np.full(k2d.shape, 1.0)  ##default all ones
+#     if ns > 1:
+#         if s == 0:
+#             resp = lowpass_resp(k2d, scales[s], scales[s+1])
+#         if s == ns-1:
+#             resp = 1 - lowpass_resp(k2d, scales[s-1], scales[s])
+#         if s > 0 and s < ns-1:
+#             resp = lowpass_resp(k2d, scales[s], scales[s+1]) - lowpass_resp(k2d, scales[s-1], scales[s])
+#     return resp
+
+
+# def get_scale_comp(fld, scales, s):
+#     ##compute scale component of fld on the grid
+#     ##given scales: list of center wavenumber k defining the scale bands
+#     ##       s: the index for scale components
+
+#     # xk = grid2spec(x)
+#     # xkout = xk.copy()
+#     # ns = len(scales)
+#     # if ns > 1:
+#     #     kx, ky = get_wn(x)
+#     #     k2d = np.sqrt(kx**2 + ky**2)
+#     #     xkout = xk * get_scale_resp(k2d, scales, s)
+#     # return spec2grid(xkout)
+#     pass
+
+
+# def convolve_fft(grid, fld, kernel):
+#     assert grid.regular, 'Convolution using FFT approach only works for regular grids'
+#     fld_spec = fft2(fld)
+#     knl_spec = fft2(kernel)
+#     mask = np.isnan(fld)
+#     fld[mask] = 0.  ##temporarily put zeros in NaN area
+
+#     fld_conv_spec = fld_spec * knl_spec
+#     fld_conv = ifft2(fld_conv_spec)
+
+#     fld_conv[mask] = np.nan
+
+#     return fld_conv
+
+
+# def convolve(grid, fld, kernel):
+#     fld_shp = fld.shape
+#     assert fld_shp == grid.x.shape, 'fld shape mismatch with grid'
+
+#     x = grid.x.flatten()
+#     y = grid.y.flatten()
+#     fld = fld.flatten()
+#     mask = np.isnan(fld)
+#     fld_conv = fld.copy()
+
+#     for i in np.where(mask):
+#         fld_conv[i]
+
+#     return fld_conv
+
+
+def get_coords(psik):
+    nkx, nky = psik.shape
+    kmax = nky-1
+    kx_, ky_ = np.mgrid[-kmax:kmax+1, 0:kmax+1]
+    return kx_, ky_
+
+def spec_bandpass(xk, krange, s):
+    kx_, ky_ = get_coords(xk)
+    Kh = np.sqrt(kx_**2 + ky_**2)
+    xkout = xk.copy()
+    r = scale_response(Kh, krange, s)
+    return xkout * r
+
+def scale_response(Kh, krange, s):
+    ns = len(krange)
+    r = np.zeros(Kh.shape)
+    center_k = krange[s]
+    if s == 0:
+        r[np.where(Kh<=center_k)] = 1.0
+    else:
+        left_k = krange[s-1]
+        ind = np.where(np.logical_and(Kh>=left_k, Kh<=center_k))
+        r[ind] = np.cos((Kh[ind] - center_k)*(0.5*np.pi/(left_k - center_k)))**2
+    if s == ns-1:
+        r[np.where(Kh>=center_k)] = 1.0
+    else:
+        right_k = krange[s+1]
+        ind = np.where(np.logical_and(Kh>=center_k, Kh<=right_k))
+        r[ind] = np.cos((Kh[ind] - center_k)*(0.5*np.pi/(right_k - center_k)))**2
     return r
 
 
-def scale_response(k2d, scales, s):
-    ns = len(scales)
-    resp = np.full(k2d.shape, 1.0)  ##default all ones
-    if ns > 1:
-        if s == 0:
-            resp = lowpass_resp(k2d, scales[s], scales[s+1])
-        if s == ns-1:
-            resp = 1 - lowpass_resp(k2d, scales[s-1], scales[s])
-        if s > 0 and s < ns-1:
-            resp = lowpass_resp(k2d, scales[s], scales[s+1]) - lowpass_resp(k2d, scales[s-1], scales[s])
-    return resp
-
-
-def get_scale_comp(fld, scales, s):
-    ##compute scale component of fld on the grid
-    ##given scales: list of center wavenumber k defining the scale bands
-    ##       s: the index for scale components
-
-    # xk = grid2spec(x)
-    # xkout = xk.copy()
-    # ns = len(scales)
-    # if ns > 1:
-    #     kx, ky = get_wn(x)
-    #     k2d = np.sqrt(kx**2 + ky**2)
-    #     xkout = xk * get_scale_resp(k2d, scales, s)
-    # return spec2grid(xkout)
-    pass
-
-
-def convolve_fft(grid, fld, kernel):
-    assert grid.regular, 'Convolution using FFT approach only works for regular grids'
-    fld_spec = fft2(fld)
-    knl_spec = fft2(kernel)
-    mask = np.isnan(fld)
-    fld[mask] = 0.  ##temporarily put zeros in NaN area
-
-    fld_conv_spec = fld_spec * knl_spec
-    fld_conv = ifft2(fld_conv_spec)
-
-    fld_conv[mask] = np.nan
-
-    return fld_conv
-
-
-def convolve(grid, fld, kernel):
-    fld_shp = fld.shape
-    assert fld_shp == grid.x.shape, 'fld shape mismatch with grid'
-
-    x = grid.x.flatten()
-    y = grid.y.flatten()
-    fld = fld.flatten()
-    mask = np.isnan(fld)
-    fld_conv = fld.copy()
-
-    for i in np.where(mask):
-        fld_conv[i]
-
-    return fld_conv
-
-
+def get_scale(fld, krange, s):
+    fldk = grid2spec(fld)
+    fldk = spec_bandpass(fldk, krange, s)
+    return spec2grid(fldk)
