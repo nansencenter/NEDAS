24c58af087258800457f09e218401392ea1b678e
Yue Ying <hardyying@gmail.com>
2024-06-14 22:22:32 +0200

    diag scripts
    plot error time series and summary, for figures that compare qg and
    qg.emulator in collab with Julien

--- diag/plot_err_ts.py
+++ diag/plot_err_ts.py
@@ -1,9 +1,15 @@
-model_name = 'qg'
-c = Config(config_file="../config/samples/"+model_name+".yml")
-c.nens = 60
-c.work_dir = "/cluster/work/users/yingyue/"+model_name+f".n{c.nens}"
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+from config import Config
+from utils.conversion import s2t, t2s, dt1h
 
-sc='_s0'
+model_name = sys.argv[1] ##'qg'
+c = Config(config_file="../config/samples/"+model_name+".yml")
+c.nens = int(sys.argv[2])  ##5
+c.work_dir = "/cluster/work/users/yingyue/nopert/"+model_name+f".n{c.nens}"
+
+sc=''
 
 c.time_start = s2t('202301010000')
 c.time_end = s2t('20230218000')
@@ -36,10 +42,9 @@ xticks = np.arange(0, (c.time_end-c.time_start)/dt1h+1, 72)
 ax.set_xticks(xticks)
 ax.set_xticklabels((xticks/24).astype(int))
 ax.set_xlabel('time (day)')
-ax.set_ylabel('m/s')
 ax.set_ylim(0, 1)
 #ax.set_xlim(0, 120)
-ax.set_title(f'{model_name} N={c.nens} {sc}')
+ax.set_title(model_name+r" $N_e$="+f"{c.nens} {sc}")
 
 plt.savefig(f'sawtooth.{model_name}.n{c.nens}{sc}.png', dpi=100)
 
