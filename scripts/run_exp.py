bfb89c081ba93606e42899c5184f1342b34145c1
Yue Ying <hardyying@gmail.com>
2024-06-28 11:17:45 +0200

    some issue remain in run_exp.py

--- scripts/run_exp.py
+++ scripts/run_exp.py
@@ -16,13 +16,6 @@ def copy_file(file1, file2):
     os.system(f"mkdir -p {os.path.dirname(file2)}; cp {file1} {file2}")
 
 
-# def perturb_member(path, model, mem_id, time, k, name=None, amp=0, hcorr=1):
-#     print('perturb', mem_id, time)
-#     fld = model.read_var(path=path, name=name, member=mem_id, time=c.time, k=k)
-#     fld += random_field_gaussian(model.grid.nx, model.grid.ny, amp, hcorr)
-#     model.write_var(fld, path=path, name=name, member=mem_id, time=c.time, k=k)
-
-
 c = Config(parse_args=True)
 c.show_summary()
 
@@ -46,6 +39,9 @@ while c.time < c.time_end:
 
     if c.time == c.time_start:
         ##copy initial ensemble
+        ##TODO: figure out a way to do this initialization in model class?
+        ##      hard-wiring this code in the top-level script is not so good
+        ##TODO: 128 processors available, but it depends on env/machine
         with ProcessPoolExecutor(max_workers=128) as executor:
             futures = []
             for mem_id in range(c.nens):
@@ -61,37 +57,6 @@ while c.time < c.time_end:
                     result = future.result()
                 except Exception as e:
                     print(f'An error occurred: {e}')
-
-        ##perturb the initial ensemble
-        # for model_name, model in c.model_config.items():
-        #     path2 = os.path.join(c.work_dir, 'cycle', t2s(c.time), model_name)
-        #     for mem_id in range(c.nens):
-        #         for k in range(model.nz):
-        #             state_init = model.read_var(path=path1, name='temperature', time=c.time, k=k)
-        #             state = state_init + random_field_gaussian(model.grid.nx, model.grid.ny, 0.1, 50)
-        #             file2 = model.filename(path=path2, member=mem_id, time=c.time)
-        #             os.system(f"mkdir -p {os.path.dirname(file2)}; touch {file2}")
-        #             model.write_var(state, path=path2, name='temperature', member=mem_id, time=c.time, k=k)
-
-    #add perturbation to the ensemble state
-    # with ProcessPoolExecutor(max_workers=4) as executor:
-    #     futures = []
-    # for model_name, model in c.model_config.items():
-    #     for mem_id in range(c.nens):
-    #         for k in range(model.nz):
-    #             path = os.path.join(c.work_dir, 'cycle', t2s(c.time), model_name)
-    #             #TODO: set perturb opt in config
-    #             perturb_opt = {'name': 'streamfunc',
-    #                         'amp': 1,
-    #                         'hcorr': 20,}
-    #             perturb_member(path, model, mem_id, c.time, k, **perturb_opt)
-    #                 future = executor.submit(perturb_member, path, model, mem_id, c.time, k, **perturb_opt)
-    #                 futures.append(future)
-    #     for future in as_completed(futures):
-    #         try:
-    #             result = future.result()
-    #         except Exception as e:
-    #             print(f'An error occurred: {e}')
 
     ###run the ensemble forecasts, use batch or scheduler approach
     for model_name, model in c.model_config.items():
