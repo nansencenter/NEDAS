7cf6559185f42ff8f4d508c46031659caeaae449
Yue Ying <hardyying@gmail.com>
2024-10-13 00:20:52 +0200

    perturb modified to allow grid convert
    perturbations are generated in analysis c.grid (always regular grid for
    now, so it is save to do gaussian_random_field there). The perturbations
    will be converted to model.grid afterwards

--- scripts/perturb.py
+++ scripts/perturb.py
@@ -1,8 +1,8 @@
 import numpy as np
 import os
 from utils.conversion import dt1h, ensure_list
-from utils.parallel import distribute_tasks, bcast_by_root, by_rank
-from utils.progress import timer, print_with_cache, progress_bar
+from utils.parallel import distribute_tasks, bcast_by_root
+from utils.progress import timer
 from utils.dir_def import forecast_dir
 from utils.random_perturb import random_perturb
 
@@ -20,14 +20,12 @@ def perturb_save_file(c, model_name, vname, time, mem_id):
     return os.path.join(path, 'perturb', vname+mstr+'.npy')
 
 def main(c):
-    print = by_rank(c.comm, c.pid_show)(print_with_cache)
-    if c.debug:
-        print('perturbing \n')
-
     task_list = bcast_by_root(c.comm)(distribute_perturb_tasks)(c)
-    nr = len(task_list[c.pid])
-    for r in range(nr):
-        rec = task_list[c.pid][r]
+
+    for rec in task_list[c.pid]:
+        model_name = rec['model_src']
+        model = c.model_config[model_name]
+
         model_name = rec['model_src']
         model = c.model_config[model_name]  ##model class object
         mem_id = rec['member']
@@ -43,38 +41,48 @@ def main(c):
             else:
                 perturb[vname] = None
 
+        ##perturb all sub time steps for variables within this cycle
+        vname =variable_list[0]  ##note: all variables in the list shall have same dt and k levels
         dt = model.variables[vname]['dt']   ##time interval for variable vname in this cycle
         nstep = c.cycle_period // dt        ##number of time steps to be perturbed
         for n in np.arange(nstep):
-            if c.debug:
-                print(progress_bar(r*nstep+n, nr*nstep))
-
             t = c.time + n * dt * dt1h
 
             for k in model.variables[vname]['levels']:
-                ##TODO: only works for surface layer variables now (forcing)
-                ##      but can be extended to 3D variables with additional vertical corr
+                #TODO: only works for surface layer variables now with k=0 (forcing variables)
+                ##      but can be extended to 3D variables with additional vertical corr parameter
+                if c.debug:
+                    print(f"PID {c.pid}: perturbing mem{mem_id+1} {variable_list} at {t} level {k}", flush=True)
 
-                ##read variable from model state
+                model.read_grid(path=path, time=t, member=mem_id, k=k)
+                model.grid.set_destination_grid(c.grid)
+                c.grid.set_destination_grid(model.grid)
+
                 fields = {}
                 for vname in variable_list:
-                    fields[vname] = model.read_var(path=path, name=vname, time=t, member=mem_id, k=k)
+                    ##read variable from model state
+                    fld = model.read_var(path=path, name=vname, time=t, member=mem_id, k=k)
+                    ##convert to analysis grid
+                    fields[vname] = model.grid.convert(fld, is_vector=model.variables[vname]['is_vector'])
 
-                ##generate perturbation
-                fields_pert, perturb = random_perturb(model.grid, fields, prev_perturb=perturb, dt=dt, **rec)
+                ##generate perturbation on analysis grid
+                fields_pert, perturb = random_perturb(c.grid, fields, prev_perturb=perturb, dt=dt, **rec)
 
-                ##write variable back to model state
-                for vname in variable_list:
-                    model.write_var(fields_pert[vname], path=path, name=vname, time=t, member=mem_id, k=k)
+                if hasattr(model, 'displace'):
+                    ##use model internal method to apply displacement perturbations directly
+                    model.displace(perturb, path=path, time=t, member=mem_id, k=k)
+                else:
+                    ##convert from analysis grid to model grid, and
+                    ##write the perturbed variables back to model state files
+                    for vname in variable_list:
+                        fld = c.grid.convert(fields_pert[vname], is_vector=model.variables[vname]['is_vector'])
+                        model.write_var(fld, path=path, name=vname, time=t, member=mem_id, k=k)
 
         ##save a copy of perturbation for later cycles
         for vname in variable_list:
             psfile = perturb_save_file(c, model_name, vname, c.time, mem_id)
             os.system(f"mkdir -p {os.path.dirname(psfile)}")
             np.save(psfile, perturb[vname])
-
-    if c.debug:
-        print('. done\n')
 
 def perturb(c):
     """run this perturb.py script in a subprocess with mpi, using all nproc cores"""
@@ -100,5 +108,5 @@ if __name__ == "__main__":
     from config import Config
     c = Config(parse_args=True)  ##get config from runtime args
 
-    main(c)
+    timer(c)(main)(c)
 
