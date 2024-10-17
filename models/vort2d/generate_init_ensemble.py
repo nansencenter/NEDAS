6c169bbf4a5ca2032ff4f6f334a07a5fbdabdf56
Yue Ying <hardyying@gmail.com>
2024-10-17 15:48:44 +0200

    more output for vort2d scripts

--- models/vort2d/generate_init_ensemble.py
+++ models/vort2d/generate_init_ensemble.py
@@ -10,7 +10,17 @@ ens_init_dir = model.ens_init_dir
 os.system("mkdir -p "+ens_init_dir)
 
 for m in range(c.nens):
+    opts = {
+        'path': ens_init_dir,
+        'name': 'velocity',
+        'is_vector': True,
+        'time': c.time,
+        'member': m,
+        }
+    print(f"generating initial condition for member {m+1}, output to {model.filename(**opts)}")
+
     state = model.generate_initial_condition()
 
-    model.write_var(state, path=ens_init_dir, name='velocity', is_vector=True, time=c.time, member=m)
+    model.write_var(state, **opts)
 
+print("done.")
--- models/vort2d/generate_truth.py
+++ models/vort2d/generate_truth.py
@@ -5,7 +5,6 @@ from utils.dir_def import forecast_dir
 
 c = Config(parse_args=True)
 
-
 model = c.model_config['vort2d']
 
 truth_dir = model.truth_dir
@@ -13,12 +12,23 @@ os.system("mkdir -p "+truth_dir)
 
 t = c.time_start
 while t < c.time_end:
+    opts = {
+        'path': truth_dir,
+        'name': 'velocity',
+        'is_vector': True,
+        'time': t,
+        }
 
     if t == c.time_start:
         state = model.generate_initial_condition()
-        model.write_var(state, path=truth_dir, name='velocity', is_vector=True, time=t)
+        print(f"generating initial condition {model.filename(**opts)}")
+        model.write_var(state, **opts)
 
+    next_t = t + c.cycle_period * dt1h
+    print(f"running model, saving output {model.filename(**{**opts, 'time':next_t})}")
     model.run(path=truth_dir, time=t, forecast_period=c.cycle_period)
 
-    t += c.cycle_period * dt1h
+    t = next_t
+
+print("done.")
 
