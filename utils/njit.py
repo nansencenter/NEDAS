c8454b4f51648f14ab5c6713463ccb65cdb81db2
Yue Ying <hardyying@gmail.com>
2024-10-17 10:36:21 +0200

    minor fixes
    numpy.fft.ifft2 returns a complex dtype, weird, we only need the real part
    
    njit decorator can be used in two modes @njit and @njit(args), so make
    the empty decorator conform

--- utils/njit.py
+++ utils/njit.py
@@ -3,13 +3,25 @@ try:
 
 except ImportError:
     print("Warning: numba is not found in your environment, will skip the njit precompiling of functions.")
+
     ##define njit as en empty decorator
     from functools import wraps
-    def njit(cache=True):
-        def decorator(func):
+
+    def njit(*njit_args, **njit_kwargs):
+        #@njit() - decorator with arguments
+        if len(njit_args) == 0:
+            def decorator(func):
+                @wraps(func)
+                def wrapper(*func_args, **func_kwargs):
+                    return func(*func_args, **func_kwargs)
+                return wrapper
+            return decorator
+
+        #@njit - decorator without arguments
+        elif callable(njit_args[0]):
+            func = njit_args[0]
             @wraps(func)
-            def wrapper(*args, **kwargs):
-                return func(*args, **kwargs)
+            def wrapper(*func_args, **func_kwargs):
+                return func(*func_args, **func_kwargs)
             return wrapper
-        return decorator
 
