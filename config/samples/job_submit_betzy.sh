65ddefc92a191e1a1aad4ddd7b11b559586e69e9
Yue Ying <hardyying@gmail.com>
2024-10-18 13:31:50 +0200

    add more samples for job_submit scripts

--- config/samples/job_submit_betzy.sh
+++ config/samples/job_submit_betzy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##this script runs parallel command on your host machine
 ##run as:
-##   job_submit.sh nproc offset job_exe_cmd
+##   job_submit.sh nproc offset exe_command
 ##arguments:
 ##   nproc = the total number of processors to run the job
 ##   offset = the starting processor index for the job
@@ -19,8 +19,8 @@ offset=$2
 shift 2
 exe_command=$@
 
-if [ -z $SLURM_TASKS_PER_NODE ]; then
-    ##running job from login node
+if [ -z '$SLURM_TASKS_PER_NODE' ]; then
+    ##running job from login node (slurm environment not found)
     ##this is not suitable for large amount of processors
     if [ "$nproc" -gt 16 ] || [ "$offset" -gt 16 ]; then
         echo "You are on login node, don't run large parallel programs, try reducing nproc below 16"
@@ -29,9 +29,21 @@ if [ -z $SLURM_TASKS_PER_NODE ]; then
     mpiexec -n $nproc $exe_command
 else
     ppn=$(echo $SLURM_TASKS_PER_NODE |awk -F'(' '{print $1}')
+    ntask_avail=$SLURM_NTASKS
+    nnode_avail=$SLURM_NNODES
 
     nnode=$(echo "($nproc+$ppn-1)/$ppn" |bc)
     offset_node=$(echo "$offset/$ppn" |bc)
+
+    nnode_req=$((offset_node+$nnode))
+    if [ $nnode_req -gt $nnode_avail ]; then
+        echo "Requested offset+nnodes=$nnode_req exceeds the available nnodes=$nnode_avail, aborting"
+        exit
+    fi
+    if [ $nproc -gt $ntask_avail ]; then
+        echo "Requested nproc=$nproc exceeds the available ntasks=$ntask_avail, aborting"
+        exit
+    fi
 
     srun -N $nnode -n $nproc -r $offset_node --exact --unbuffered $exe_command
 fi
