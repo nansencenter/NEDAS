7e3a7b446043d4aaa82df74efbc81b75db60a393
Yue Ying <hardyying@gmail.com>
2024-12-06 23:28:29 +0100

    SlurmJobSubmitter added job array support

--- utils/job_submitters/slurm.py
+++ utils/job_submitters/slurm.py
@@ -31,9 +31,9 @@ class SLURMJobSubmitter(JobSubmitter):
     @property
     def execute_command(self):
         if self.run_separate_jobs:
-            return f"srun --unbuffered"
+            return f"srun -n {self.nproc} --unbuffered"
         else:
-            return f"srun -N {self.nproc} -n {self.nnode} -r {self.offset_node} --exact --unbuffered"
+            return f"srun -n {self.nproc} -N {self.nnode} -r {self.offset_node} --exact --unbuffered"
 
     @property
     def job_array_index_name(self):
@@ -47,14 +47,17 @@ class SLURMJobSubmitter(JobSubmitter):
             job_script.write(f"#SBATCH --job-name={self.job_name}\n")
             job_script.write(f"#SBATCH --account={self.project}\n")
             job_script.write(f"#SBATCH --time={seconds_to_timestr(self.walltime)}\n")
-            job_script.write(f"#SBATCH --qos={self.queue}\n")
             job_script.write(f"#SBATCH --nodes={self.nnode}\n")
             job_script.write(f"#SBATCH --ntasks-per-node={self.ppn}\n")
-
+            if self.queue and self.queue != 'normal':
+                job_script.write(f"#SBATCH --qos={self.queue}\n")
             if self.mem_per_cpu:
                 job_script.write(f"#SBATCH --mem-per-cpu={self.mem_per_cpu}\n")
 
-            log_file = os.path.join(self.run_dir, f"{self.job_name}-%j.out")
+            if self.use_job_array:
+                log_file = os.path.join(self.run_dir, f"{self.job_name}-%A_%a.out")
+            else:
+                log_file = os.path.join(self.run_dir, f"{self.job_name}-%j.out")
             job_script.write(f"#SBATCH --output={log_file}\n")
 
             if self.use_job_array:
@@ -67,30 +70,48 @@ class SLURMJobSubmitter(JobSubmitter):
 
             self.job_script = job_script.name
 
-        os.system("cat "+self.job_script)
-
         ##submit the job script
         p = subprocess.run(['sbatch', self.job_script], capture_output=True, text=True)
         if p.returncode != 0:
             raise RuntimeError(f"Failed to submit job: {p.stderr}")
         self.job_id = int(p.stdout.split()[-1])
-        log_file = os.path.join(self.run_dir, f"{self.job_name}-{self.job_id}.out")
 
         ##monitor job status
-        while True:
-            sleep(20)
-            p = subprocess.run(['squeue', '-h', '-j', f'{self.job_id}'], capture_output=True, text=True)
-            if not p.stdout:
-                ##job no longer in queue
-                break
-            job_status = p.stdout.split()[4]
-            if job_status not in ['R', 'PD', 'CG']:
-                ##job not running, pending, or cleaning up
-                raise RuntimeError(f"job {self.job_name} failed with status {job_status}")
+        if self.use_job_array:
+            while True:
+                sleep(20)
+                job_finished = []
+                for i in range(self.array_size):
+                    p = subprocess.run(['squeue', '-h', '-j', f'{self.job_id}_{i}'], capture_output=True, text=True)
+                    if not p.stdout:
+                        job_finished.append(True)
+                    else:
+                        job_finished.append(False)    
+                if all(job_finished):
+                    break
+                        
+        else:
+            while True:
+                sleep(20)
+                p = subprocess.run(['squeue', '-h', '-j', f'{self.job_id}'], capture_output=True, text=True)
+                if not p.stdout:
+                    ##job no longer in queue
+                    break
+                job_status = p.stdout.split()[4]
+                if job_status not in ['R', 'PD', 'CG']:
+                    ##job not running, pending, or cleaning up
+                    raise RuntimeError(f"job {self.job_name} failed with status {job_status}")
 
         ##clean up
         os.remove(self.job_script)
 
-        ##handle errors
-        if not find_keyword_in_file(log_file, "Job exited normally"):
-            raise RuntimeError(f"job {self.job_name} failed, check {log_file}")
+        ##check log file and report errors
+        if self.use_job_array:
+            for i in range(self.array_size):
+                log_file = os.path.join(self.run_dir, f"{self.job_name}-{self.job_id}_{i}.out")
+                if not find_keyword_in_file(log_file, "Job exited normally"):
+                    raise RuntimeError(f"job {self.job_name} failed, check {log_file}")
+        else:
+            log_file = os.path.join(self.run_dir, f"{self.job_name}-{self.job_id}.out")
+            if not find_keyword_in_file(log_file, "Job exited normally"):
+                raise RuntimeError(f"job {self.job_name} failed, check {log_file}")
