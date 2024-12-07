a8c9757c1e977a9d270dab43a01ea8997f3b65fd
Yue Ying <hardyying@gmail.com>
2024-12-07 20:22:51 +0100

    nextsim.dg.model.preprocess fixes
    combined prepare_restart and prepare_forcing into preprocess
    
    since nextsim.dg config_file is holding the files and perturb options,
    they are initialized directly in model class, instead of passing through
    kwargs everytime.

--- models/nextsim/dg/model.py
+++ models/nextsim/dg/model.py
@@ -1,12 +1,9 @@
 import numpy as np
 import os
 import subprocess
-from datetime import datetime
-from time import sleep
 
-from config import parse_config
 from utils.conversion import units_convert, dt1h
-from utils.shell_utils import run_command, run_job, makedir
+from utils.shell_utils import run_job, makedir
 from . import restart, forcing, namelist
 from ...model_config import ModelConfig
 
@@ -18,9 +15,12 @@ class Model(ModelConfig):
         self.variables = {  ##TODO: a list of model variables defined here
              'seaice_conc': {'name':'M_conc', 'dtype':'float', 'is_vector':False, 'dt':self.restart_dt, 'levels':[0], 'units':'%'},
              }
+        # restart
+        # forcing
+        
         self.z_units = 'm'
 
-        #self.grid = 
+        #self.grid construct grid obj based on config
 
     def filename(self, **kwargs):
         kwargs = super().parse_kwargs(**kwargs)
@@ -63,79 +63,91 @@ class Model(ModelConfig):
         return np.zeros(self.grid.x.shape)
 
     def preprocess(self, task_id:int=0, **kwargs):
-        """Preprocessing method for nextsim.dg"""
-        self.prepare_restart(task_id, **kwargs)
-        self.prepare_forcing(task_id, **kwargs)
-
-    def prepare_restart(self, task_id:int=0, **kwargs):
-        """generate initial condition for a single ensemble member.
-
+        """Preprocessing method for nextsim.dg
         Parameters
         ----------
         task_id : int
             task id for parallel execution
-        task_nproc : int
+        self.nproc_per_util : int
             number of processors for each task
         **kwargs : dict
             keyword arguments for the model configuration
             Keywords defined when the function is called:
             - member : int
                 ensemble member id
-            - time_start : datetime
+            - time : datetime
                 start time of the forecast
             - path : str
                 path to the working directory of the ensemble member
-            These are defined in the configuration file of model_def/nextsim.dg section
-            - files : dict
+        These are defined in the configuration file model_def:nextsim.dg section,
+        and parse_config will bring them in this class as
+            - self.files : dict
                 This section contains the filenames for the restart file.
                 This must have a `restart` key that defines the filename `format`
                 and the strftime `time_format` code of the restart filename.
                 If perturbation is used, this section must also have a `lon_name`
                 and `lat_name` key
-            - perturb : dict
+            - self.perturb : dict
                 perturbation options for the initial conditions.
                 See example configuration file for required keys and explanations.
                 This section is not necessary if the model does not use perturbation
                 for the initial conditions or forcings.
-
         """
+        kwargs = super().parse_kwargs(**kwargs)
+
         # get the current ensemble member id
         ens_mem_id:int = kwargs['member'] + 1
         # ensemble member directory for the current member
-        ens_init_dir:str = os.path.join(kwargs['path'],
+        ens_mem_dir:str = os.path.join(kwargs['path'],
                                         f'ens_{str(ens_mem_id).zfill(2)}')
-        # create directory for the initial ensemble
-        os.makedirs(ens_init_dir, exist_ok=True)
+        # create directory for the ensemble member
+        os.makedirs(ens_mem_dir, exist_ok=True)
+        
         # get all required filenames for the initial ensemble
-        # 1. get current time, which is supposed to be the start time
-        time = kwargs['time_start']
-        # 2. get the restart filename
-        file_options_restart = kwargs['files']['restart']
+        # 1. get current and next time
+        time = kwargs['time']
+        forecast_period = kwargs['forecast_period']
+        next_time = time + forecast_period * dt1h
+
+        # 2. get the restart and forcing filename
+        file_options_restart = self.files['restart']
         fname_restart:str = restart.get_restart_filename(file_options_restart, ens_mem_id, time)
 
+        file_options_forcing:dict[str, str] = self.files['forcing']
+        fname_forcing:dict[str, str] = dict()
+        for forcing_name in file_options_forcing:
+            fname_forcing[forcing_name] = forcing.get_forcing_filename(file_options_forcing[forcing_name], ens_mem_id, time)
+
         # no need for perturbation if not specified in yaml file
-        if 'perturb' not in kwargs:
-            print ('We do no perturbations as perturb section is not specified in the model configuration.')
+        if not hasattr(self, 'perturb'):
+            print ('We do no perturbations as perturb section is not specified in the model configuration.', flush=True)
             # we we do not perturb the restart file
             # simply link the restart files
-            os.system(f'ln -s {fname_restart} {ens_init_dir}')
+            os.system(f'ln -s {fname_restart} {ens_mem_dir}')
+            # we we do not perturb the forcing file
+            # simply link the forcing files
+            for forcing_name in file_options_forcing:
+                os.system(f'ln -s {fname_forcing[forcing_name]} {ens_mem_dir}')
             return
-
-        # get perturbation options
-        perturb_options = kwargs['perturb']
 
         # here, if 'restart section is not under perturb section
         # we only link the restart file to each ensemble directory
-        if 'restart' not in perturb_options:
+        if 'restart' not in self.perturb:
             # we we do not perturb the restart file
             # simply link the restart files
-            os.system(f'ln -s {fname_restart} {ens_init_dir}')
+            os.system(f'ln -s {fname_restart} {ens_mem_dir}')
+            return
+        if 'forcing' not in self.perturb:
+            # we we do not perturb the forcing file
+            # simply link the forcing files
+            for forcing_name in file_options_forcing:
+                os.system(f'ln -s {fname_forcing[forcing_name]} {ens_mem_dir}')
             return
 
         # 3. add perturbations
-        restart_options = perturb_options['restart']
+        restart_options = self.perturb['restart']
         # copy restart files to the ensemble member directory
-        fname = os.path.join(ens_init_dir, os.path.basename(fname_restart))
+        fname = os.path.join(ens_mem_dir, os.path.basename(fname_restart))
         subprocess.run(['cp', '-v', fname_restart, fname])
         # prepare the restart file options for the perturbation
         file_options = {'fname': fname,
@@ -144,86 +156,16 @@ class Model(ModelConfig):
         # perturb the restart file
         restart.perturb_restart(restart_options, file_options)
 
-    def prepare_forcing(self, task_id:int, **kwargs):
-        """Prepare forcing file for the next forecast for a single ensemble member.
-
-        Parameters
-        ----------
-        task_id : int
-            task id for parallel execution
-        **kwargs : dict
-            keyword arguments for the model configuration
-            Keywords defined when the function is called:
-            - member : int
-                ensemble member id
-            - time : datetime
-                start time of the forecast
-            - next_time : datetime
-                end time of the forecast
-            - path : str
-                path to the working directory of the ensemble member
-            These are defined in the configuration file of model_def/nextsim.dg section
-            - files : dict
-                This section contains the filenames for the forcing file.
-                This must have a `forcing` key with each subsection the
-                atmosphere/ocean. Within these subsections, the filename `format`
-                and the strftime `time_format` code of the restart filename.
-                If perturbation is used, this section must also have a `lon_name`
-                and `lat_name` key
-            - perturb : dict
-                perturbation options for the initial conditions.
-                See example configuration file for required keys and explanations.
-                This section is not necessary if the model does not use perturbation
-                for the initial conditions or forcings.
-
-        """
-        # get the current ensemble member id
-        ens_mem_id:int = kwargs['member'] + 1
-        # ensemble member directory for the current member
-        ens_init_dir:str = os.path.join(kwargs['path'],
-                                        f'ens_{str(ens_mem_id).zfill(2)}')
-        # create directory for the initial ensemble
-        os.makedirs(ens_init_dir, exist_ok=True)
-        # get all required filenames for the initial ensemble
-        # 1. get current time and the end time of the forecast
-        time:datetime = kwargs['time']
-        next_time:datetime = kwargs['next_time']
-        # 2. get the forcing filenames
-        file_options_forcing:dict[str, str] = kwargs['files']['forcing']
-        fname_forcing:dict[str, str] = dict()
-        for forcing_name in file_options_forcing:
-            fname_forcing[forcing_name] = forcing.get_forcing_filename(file_options_forcing[forcing_name],
-                                                         ens_mem_id, time)
-
-        if 'perturb' not in kwargs:
-            print ('We do no perturbations as perturb section is not specified in the model configuration.',
-                   flush=True)
-            # we we do not perturb the forcing file
-            # simply link the forcing files
-            for forcing_name in file_options_forcing:
-                os.system(f'ln -s {fname_forcing[forcing_name]} {ens_init_dir}')
-            return
-
-        # get perturbation options
-        perturb_options = kwargs['perturb']
-
-        if 'forcing' not in perturb_options:
-            # we we do not perturb the forcing file
-            # simply link the forcing files
-            for forcing_name in file_options_forcing:
-                os.system(f'ln -s {fname_forcing[forcing_name]} {ens_init_dir}')
-            return
-
-        forcing_options = perturb_options['forcing']
+        forcing_options = self.perturb['forcing']
         # construct file options for forcing
         file_options:dict = dict()
         for forcing_name in forcing_options:
             # we ignore entries that are not in the files options
             # e.g., path
             if forcing_name not in fname_forcing: continue
-            fname = os.path.join(ens_init_dir,
-                                    os.path.basename(fname_forcing[forcing_name])
-                                    )
+            fname = os.path.join(ens_mem_dir,
+                                os.path.basename(fname_forcing[forcing_name])
+                                )
             # the forcing file options for the perturbation
             file_options[forcing_name] = {'fname_src': fname_forcing[forcing_name],
                                            'fname': fname,
