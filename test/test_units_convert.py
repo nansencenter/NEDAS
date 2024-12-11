09fe936e663d54f89e430e317328616950761bec
Yue Ying <hardyying@gmail.com>
2024-12-11 14:35:51 +0100

    prepare_forcing script for ecmwf forecasts

---
+++ test/test_units_convert.py
@@ -0,0 +1,24 @@
+import numpy as np
+import unittest
+from utils.conversion import units_convert
+
+class TestUnitsConvert(unittest.TestCase):
+
+    def test_units_convert_scalar(self):
+        self.assertEqual(units_convert(100, 1, 1.), 0.01)
+        self.assertEqual(units_convert('km', 'm', 1.), 1000.)
+        self.assertEqual(units_convert('h', 's', 1.), 3600.)
+        self.assertEqual(units_convert('m/s', 'km/h', 1.), 3.6)
+        self.assertEqual(units_convert('kg', 'g', 1.), 1000.)
+        self.assertEqual(units_convert('h', 's', 1.), 3600.)
+        self.assertEqual(units_convert('h', 's', 1.), 3600.)
+        self.assertEqual(units_convert('h', 's', 1.), 3600.)
+
+    def test_units_convert_field(self):
+        shape = (100, 100)
+        self.assertEqual(np.mean(units_convert(100, 1, np.ones(shape))), 0.01)
+        self.assertAlmostEqual(np.mean(units_convert('km', 'm', np.ones(shape))), 1000.)
+
+
+if __name__ == '__main__':
+    unittest.main()
