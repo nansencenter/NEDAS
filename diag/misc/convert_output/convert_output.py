dcf112ea3c329a5e4ab29f5d6a608034c262637e
Yue Ying <hardyying@gmail.com>
2024-12-15 09:15:24 +0100

    diag.misc.convert_output update

--- diag/misc/convert_output/convert_output.py
+++ diag/misc/convert_output/convert_output.py
@@ -23,10 +23,6 @@ def run(c, **kwargs):
 
     variables = ensure_list(kwargs['variables'])
 
-    var_names = ensure_list(kwargs['var_names'])
-    var_standard_names = ensure_list(kwargs['var_standard_names'])
-    var_units = ensure_list(kwargs['var_units'])
-
     member = kwargs['member']
 
     model_name = kwargs['model_src']
@@ -51,36 +47,33 @@ def run(c, **kwargs):
 
     model.grid.set_destination_grid(grid)
 
-    if 'time_start' in kwargs:
-        time_start = s2t(kwargs['time_start'])
+    if 'time' in kwargs:
+        time_start = s2t(kwargs['time'])
     else:
-        time_start = c.time_start
-    if 'time_end' in kwargs:
-        time_end = s2t(kwargs['time_end'])
-    else:
-        time_end = c.time_end
-    dt = c.cycle_period * dt1h
+        time_start = c.time
+    dt_hours = kwargs.get("dt_hours", model.output_dt)
+    forecast_hours = kwargs.get("forecast_hours", c.cycle_period)
     time_units = kwargs.get('time_units', 'seconds since 1970-01-01T00:00:00+00:00')
     time_calendar = kwargs.get('time_calendar', 'standard')
+    t_steps = range(0, forecast_hours, dt_hours)
+    path = forecast_dir(c, time_start, model_name)
 
-    t = time_start
-    while t <= time_end:
-        path = forecast_dir(c, t, model_name)
+    for t_step in t_steps:
+        t = time_start + t_step * dt1h
 
-        if c.debug:
-            print(f"PID {c.pid} running misc.convert_output on variables {variables} for {model_name} member {member+1} at {t}", flush=True)
-
-        for v_id, vname in enumerate(variables):
+        for vname in variables:
+            if c.debug:
+                print(f"PID {c.pid} running misc.convert_output on variable '{vname}' for {model_name} member {member+1} at {t}", flush=True)
             # read the variable from the model restart file
             rec = model.variables[vname]
-            file = kwargs['file'].format(time=t, var_name=var_names[v_id], member=member+1)
+            file = kwargs['file'].format(time=time_start, member=member+1)
             makedir(os.path.dirname(file))
 
             levels = rec['levels']
             is_vector = rec['is_vector']
             for k in levels:
                 # read the field from model restart file
-                fld = model.read_var(path=path, name=vname, time=t, member=member)
+                fld = model.read_var(path=path, name=vname, time=t, member=member, k=k)
                 # convert to output grid
                 fld_ = model.grid.convert(fld, is_vector=is_vector)
                 # build dimension records
@@ -88,8 +81,8 @@ def run(c, **kwargs):
                 time_name = kwargs.get('time_name', 'time')
                 dims[time_name] = None  ##make time dimension unlimited in nc file
                 k_name = kwargs.get('k_name')
-                if k_name: ##add multi-level field index
-                    dims[k_name] = len(levels)
+                if len(levels) > 1:
+                    dims[k_name] = None
                 x_name = kwargs.get('x_name', 'x')
                 y_name = kwargs.get('y_name', 'y')
                 dims[y_name] = grid.ny
@@ -97,28 +90,29 @@ def run(c, **kwargs):
                 lon_name = kwargs.get('lon_name', 'lon')
                 lat_name = kwargs.get('lat_name', 'lat')
                 # output the variable
-                recno = {time_name:0}  ##TODO:only process analysis time output, no forecast lead times yet
+                recno = {}
+                recno[time_name] = t_step
+                if len(levels) > 1:
+                    recno[k_name] = k
                 # variable attr
-                attr = {'standard_name':var_standard_names[v_id],
-                        'units':var_units[v_id],
+                attr = {'standard_name':vname,
+                        'units':rec['units'],
                         'grid_mapping': proj_params['projection'],
                         'coordinates': f"{lon_name} {lat_name}",
                         }
                 if is_vector:
                     for i in range(2):
-                        nc_write_var(file, dims, var_names[v_id][i], fld_[i,...], recno=recno, attr=attr, comm=c.comm)
+                        nc_write_var(file, dims, rec['name'][i], fld_[i,...], recno=recno, attr=attr)
                 else:
-                    nc_write_var(file, dims, var_names[v_id], fld_, recno=recno, attr=attr, comm=c.comm)
+                    nc_write_var(file, dims, rec['name'], fld_, recno=recno, attr=attr)
 
             # output the dimension variables
             time = cftime.date2num(t, units=time_units)
             time_attr = {'long_name': 'forecast time', 'units': time_units, 'calendar': time_calendar}
-            nc_write_var(file, {time_name:None}, time_name, time, recno=recno, attr=time_attr, comm=c.comm)
+            nc_write_var(file, {time_name:None}, time_name, time, recno=recno, attr=time_attr)
             nc_write_var(file, {x_name:grid.nx}, x_name, x, attr={'standard_name':'projection_x_coordinate', 'units':'100 km'})
             nc_write_var(file, {y_name:grid.ny}, y_name, y, attr={'standard_name':'projection_y_coordinate', 'units':'100 km'})
             nc_write_var(file, {y_name:grid.ny, x_name:grid.nx}, lon_name, lon, attr={'standard_name':'longitude', 'units':'degrees_east'})
             nc_write_var(file, {y_name:grid.ny, x_name:grid.nx}, lat_name, lat, attr={'standard_name':'latitude', 'units':'degrees_north'})
             # output projection info
             nc_write_var(file, {}, proj_params['projection'], 1, attr=proj_params)
-
-        t += dt
