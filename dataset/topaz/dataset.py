f062d4e399f1215c535e543881dfe4da31905033
Yue Ying <hardyying@gmail.com>
2024-12-19 07:21:38 +0100

    add obs_operator for topaz seaice_drift

--- dataset/topaz/dataset.py
+++ dataset/topaz/dataset.py
@@ -2,7 +2,6 @@ import numpy as np
 import os
 import glob
 from utils.conversion import dt1h
-from models.topaz.model_grid import get_topaz_grid
 from models.topaz.time_format import datetojul
 from .uf_data import read_uf_data
 from ..dataset_config import DatasetConfig
@@ -18,12 +17,15 @@ class Dataset(DatasetConfig):
             'ocean_temp': {'name':'TEM', 'dtype':'float', 'is_vector':False, 'z_units':'m', 'units':'K'},
             'ocean_saln': {'name':'SAL', 'dtype':'float', 'is_vector':False, 'z_units':'m', 'units':'psu'},
             'ocean_surf_temp': {'name':'SST', 'dtype':'float', 'is_vector':False, 'z_units':'m', 'units':'K'},
-            'ocean_surf_height': {'name':'TSLA', 'dtype':'float', 'is_vector':False, 'z_units':'m', 'units':'m'},
+            'ocean_surf_height_anomaly': {'name':'TSLA', 'dtype':'float', 'is_vector':False, 'z_units':'m', 'units':'m'},
             'seaice_conc': {'name':'ICEC', 'dtype':'float', 'is_vector':False, 'z_units':'m', 'units':'%'},
             'seaice_thick': {'name':'HICE', 'dtype':'float', 'is_vector':False, 'z_units':'m', 'units':'m'},
             'seaice_drift': {'name':'IDRFT', 'dtype':'float', 'is_vector':True, 'z_units':'m', 'units':'km'},
             }
-        self.z_units = 'm'
+        
+        self.obs_operator = {
+            'seaice_drift': self.get_seaice_drift,
+            }
 
     def filename(self, **kwargs):
         kwargs = super().parse_kwargs(**kwargs)
@@ -35,7 +37,7 @@ class Dataset(DatasetConfig):
         dirname = vname
         native_name = vname
         if name == 'seaice_drift':
-            native_name += '1'   ##i=1, 2, 3, 4, 5: 2day drift in km with starting day = current day -i-1
+            native_name += '?'   ##i=1, 2, 3, 4, 5: 2day drift in km with starting day = current day -i-1
 
         file_list = []
         if time is not None:
@@ -48,10 +50,14 @@ class Dataset(DatasetConfig):
         assert len(file_list)>0, 'no matching files found: '+search
         return file_list
 
+    def random_network(self, **kwargs):
+        raise NotImplementedError('random_network: ERROR: random_network not implemented for topaz dataset')
+
     def read_obs(self, **kwargs):
         kwargs = super().parse_kwargs(**kwargs)
         name = kwargs['name']
-        model_grid = kwargs['grid']
+        time = kwargs['time']
+        grid = kwargs['grid']
 
         is_vector = self.variables[name]['is_vector']
         obs_seq = {'obs':[], 't':[], 'z':[], 'y':[], 'x':[], 'err_std':[], }
@@ -72,16 +78,24 @@ class Dataset(DatasetConfig):
             else:
                 nobs = len(data)
 
+            # if ice drift obs, get the time lag from file name
+            obsType = os.path.basename(file).split('_')[1]
+            if obsType[:5] == 'IDRFT':
+                lag_days = int(obsType[5]) + 1
+                t = time - dt1h * 24 * lag_days   ##start time of the drift vectors
+            else:
+                t = time
+
             for i in range(nobs):
                 if is_vector:
                     obs = [data[i][0], data[nobs+i][0]]
                 else:
                     obs = data[i][0]
                 obs_seq['obs'].append(obs)
-                obs_seq['t'].append(kwargs['time'])
+                obs_seq['t'].append(t)
                 obs_seq['z'].append(-data[i][5])  ##depth
                 lon, lat = data[i][3], data[i][4]
-                x, y = model_grid.proj(lon, lat)
+                x, y = grid.proj(lon, lat)
                 obs_seq['y'].append(y)
                 obs_seq['x'].append(x)
                 obs_seq['err_std'].append(np.sqrt(data[i][1]))
@@ -91,5 +105,44 @@ class Dataset(DatasetConfig):
 
         if is_vector:
             obs_seq['obs'] = obs_seq['obs'].T  ##make vector dimension [2,nobs]
+        return obs_seq
+
+    def get_seaice_drift(self, **kwargs):
+        kwargs = super().parse_kwargs(**kwargs)
+        grid = kwargs['grid']  ##output grid
+        path = kwargs['path']
+        member = kwargs['member']
+        model = kwargs['model']  ##model object
+        model.grid.set_destination_grid(grid)
+
+        start_x = kwargs['x']
+        start_y = kwargs['y']
+        start_t = kwargs['t']
+        x = start_x.copy()
+        y = start_y.copy()
+        obs_seq = np.zeros((2,)+x.shape)
+
+        ##go through each of the 5 start times
+        for uniq_start_t in np.unique(start_t):
+            ind = np.where(start_t==uniq_start_t)
+
+            for day in range(2):
+                t = uniq_start_t + day * dt1h * 24
+
+                ##get daily mean seaice velocity
+                model_vel = model.read_var(path=path, name='seaice_velocity_daily', time=t, member=member, units='km/day')
+                ##convert to grid
+                vel = model.grid.convert(model_vel, is_vector=True)
+                ##get velocity at x, y position
+                u = grid.interp(vel[0,...], x[ind], y[ind])  ##km over 1 day
+                v = grid.interp(vel[1,...], x[ind], y[ind])
+
+                ##increment the x,y components
+                x[ind] += u * 1000.
+                y[ind] += v * 1000.
+
+        obs_seq[0, :] = (x - start_x) / 1000.
+        obs_seq[1, :] = (y - start_y) / 1000.
+
         return obs_seq
 
