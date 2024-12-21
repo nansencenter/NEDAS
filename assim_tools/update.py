7b9fdec7d8a0d90311b2016ca057bcd2abfb0459
Yue Ying <hardyying@gmail.com>
2024-12-21 21:26:20 +0100

    upgrade file lock in Comm
    The old one works only with shared memory multiprocessing. Upgraded the file lock to use truely rma parallelism, so that it works for all mpi scenarios
    
    The acquire_file_lock function needs a collective call to generate the MPI.Win object correctly, but in update.py sometimes the processor can be idle (no tasks assigned to it), so it will break the synchronization. Adding an explicit allgather for the filenames and initialize the locks properly.

--- assim_tools/update.py
+++ assim_tools/update.py
@@ -31,52 +31,61 @@ def update_restart(c, fields_prior, fields_post):
 
     ##process the fields, each processor goes through its own subset of
     ##mem_id,rec_id simultaneously
-    nm = len(c.mem_list[c.pid_mem])
-    nr = len(c.rec_list[c.pid_rec])
-
-    for r, rec_id in enumerate(c.rec_list[c.pid_rec]):
-        rec = c.state_info['fields'][rec_id]
-
-        for m, mem_id in enumerate(c.mem_list[c.pid_mem]):
-            if c.debug:
-                print(f"PID {c.pid}: update_restart mem{mem_id+1:03d} {rec}", flush=True)
+    ##but need to keep every rank in sync to coordinate multiprocess file access
+    nm_max = np.max([len(lst) for _,lst in c.mem_list.items()])
+    nr_max = np.max([len(lst) for _,lst in c.rec_list.items()])
+    for r in range(nr_max):
+        for m in range(nm_max):
+            pid_active = ( m < len(c.mem_list[c.pid_mem]) and r < len(c.rec_list[c.pid_rec]) )
+            if pid_active:
+                mem_id = c.mem_list[c.pid_mem][m]
+                rec_id = c.rec_list[c.pid_rec][r]
+                rec = c.state_info['fields'][rec_id]
+                path = forecast_dir(c, rec['time'], rec['model_src'])
+                model = c.model_config[rec['model_src']]
+                model.read_grid(path=path, member=mem_id, **rec)
+                file = model.filename(path=path, member=mem_id, **rec)
             else:
-                print_1p(progress_bar(m*nr+r, nm*nr))
+                file = None
+            all_files = c.comm.allgather(file)
+            for file in all_files:
+                c.comm.init_file_lock(file)
+            
+            if pid_active:                
+                if c.debug:
+                    print(f"PID {c.pid}: update_restart mem{mem_id+1:03d} '{rec['name']}' {rec['time']} k={rec['k']}", flush=True)
+                else:
+                    print_1p(progress_bar(m*nr_max+r, nm_max*nr_max))
 
-            ##directory storing model output
-            path = forecast_dir(c, rec['time'], rec['model_src'])
+                c.grid.set_destination_grid(model.grid)
 
-            ##load the module for handling source model
-            model = c.model_config[rec['model_src']]
-            model.read_grid(path=path, member=mem_id, **rec)
-            c.grid.set_destination_grid(model.grid)
+                fld_prior = fields_prior[mem_id, rec_id]
+                fld_post = fields_post[mem_id, rec_id]
 
-            fld_prior = fields_prior[mem_id, rec_id]
-            fld_post = fields_post[mem_id, rec_id]
+                ##analysis increment
+                fld_incr = fld_post - fld_prior
 
-            ##analysis increment
-            fld_incr = fld_post - fld_prior
+                ##misc. inverse transform
+                ##e.g. multiscale approach: just use the analysis increment directly
 
-            ##misc. inverse transform
-            ##e.g. multiscale approach: just use the analysis increment directly
+                ##convert the posterior variable back to native model grid
+                var_prior = model.read_var(path=path, member=mem_id, **rec)
+                var_post = var_prior + c.grid.convert(fld_incr, is_vector=rec['is_vector'], method='linear')
 
-            ##convert the posterior variable back to native model grid
-            var_prior = model.read_var(path=path, member=mem_id, **rec)
-            var_post = var_prior + c.grid.convert(fld_incr, is_vector=rec['is_vector'], method='linear')
+                ##post-processing by model module
+                # if hasattr(model, 'postproc'):
+                #     var_post = model.postproc(var_post, **rec)
 
-            ##post-processing by model module
-            # if hasattr(model, 'postproc'):
-            #     var_post = model.postproc(var_post, **rec)
+                ##TODO: temporary solution for nan values due to interpolation
+                # ind = np.where(np.isnan(var_post))
+                # var_post[ind] = var_prior[ind]
+                # if np.isnan(var_post).any():
+                #     raise ValueError('nan detected in var_post')
 
-            ##TODO: temporary solution for nan values due to interpolation
-            # ind = np.where(np.isnan(var_post))
-            # var_post[ind] = var_prior[ind]
-            # if np.isnan(var_post).any():
-            #     raise ValueError('nan detected in var_post')
-
-            ##write the posterior variable to restart file
-            model.write_var(var_post, path=path, member=mem_id, comm=c.comm, **rec)
+                ##write the posterior variable to restart file
+                model.write_var(var_post, path=path, member=mem_id, comm=c.comm, **rec)
 
     c.comm.Barrier()
+    c.comm.cleanup_file_locks()
     print_1p(' done.\n')
 
