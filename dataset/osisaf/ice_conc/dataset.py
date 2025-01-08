bug in units
wrong units in ocean_surf_temp caused a lot of trouble.

nondimensional unit uses numerical input (1 for conc from 0-1, can convert to 100 if you want the percentage)

don't lead the units blank, it causes trouble in read_state_info and read_obs_info, which are based on string entries separated by spaces.

dataset/osisaf/ice_conc/dataset.py
@@ -12,7 +12,7 @@ class Dataset(DatasetConfig):
     def __init__(self, config_file=None, parse_args=False, **kwargs):
         super().__init__(config_file, parse_args, **kwargs)
 
-        self.variables = {'seaice_conc': {'dtype':'float', 'is_vector':False, 'z_units':'m', 'units':'%'}, }
+        self.variables = {'seaice_conc': {'dtype':'float', 'is_vector':False, 'z_units':'m', 'units':1}, }
 
         proj = pyproj.Proj(self.proj)
         x, y = np.meshgrid(np.arange(self.xstart, self.xend, self.dx), np.arange(self.ystart, self.yend, self.dy))