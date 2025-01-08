bug in units
wrong units in ocean_surf_temp caused a lot of trouble.

nondimensional unit uses numerical input (1 for conc from 0-1, can convert to 100 if you want the percentage)

don't lead the units blank, it causes trouble in read_state_info and read_obs_info, which are based on string entries separated by spaces.

models/nextsim/dg/model.py
@@ -14,7 +14,7 @@ class Model(ModelConfig):
 
         level_sfc = np.array([0])
         self.seaice_variables = {  ##TODO: check units here and any missing variables
-             'seaice_conc': {'name':'cice', 'dtype':'float', 'is_vector':False, 'dt':self.restart_dt, 'levels':[0], 'units':'%'},
+             'seaice_conc': {'name':'cice', 'dtype':'float', 'is_vector':False, 'dt':self.restart_dt, 'levels':[0], 'units':1},
              'seaice_thick': {'name':'hice', 'dtype':'float', 'is_vector':False, 'dt':self.restart_dt, 'levels':[0], 'units':'m'},
              'snow_thick': {'name':'hsnow', 'dtype':'float', 'is_vector':False, 'dt':self.restart_dt, 'levels':[0], 'units':'m'},
              'seaice_temp': {'name':'tice', 'dtype':'float', 'is_vector':False, 'dt':self.restart_dt, 'levels':[0], 'units':'K'},