053307b7234a46614262017b519800284cc1307c
Yue Ying <hardyying@gmail.com>
2025-04-10 23:55:42 +0200

    packaging scripts

---
+++ setup.py
@@ -0,0 +1,11 @@
+[metadata]
+description-file = README.md
+
+[options]
+packages = find:
+package_dir =
+    =nedas
+include_package_data = True
+install_requires =
+    numpy>=1.18.0
+    # other dependencies from your requirements.txt
