35207fec55ec75c1e6470572f373bcedae2675f6
Yue Ying <hardyying@gmail.com>
2025-05-06 15:28:06 +0200

    make Docker image smaller

--- examples/qgmodel_benchmark/prepare_files.sh
+++ examples/qgmodel_benchmark/prepare_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python -m NEDAS.models.qg.generate_truth -c /work/config.yml
+python -m NEDAS.models.qg.generate_truth -c /app/config.yml
 
-python -m NEDAS.models.qg.generate_init_ensemble -c /work/config.yml
+python -m NEDAS.models.qg.generate_init_ensemble -c /app/config.yml
--- examples/qgmodel_benchmark/run_expt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -m NEDAS -c /work/config.yml
