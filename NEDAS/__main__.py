24b929956bc72491d43d9e90cf1e6f7754ffd546
Yue Ying <hardyying@gmail.com>
2025-05-08 00:24:18 +0200

    NEDAS.__main__ clean up
    run a single step is a functionality only for offline_filter,
    so moving the code into offline_filter __call__ function to make
    NEDAS.__main__ generally just call scheme(c)

--- NEDAS/__main__.py
+++ NEDAS/__main__.py
@@ -5,14 +5,7 @@ def main():
     print("Running NEDAS")
     c = Config(parse_args=True)
     scheme = get_analysis_scheme(c)
-    if c.step:
-        if c.step in ['perturb', 'filter', 'diagnose']:
-            mpi = True
-        else:
-            mpi = False
-        scheme.run_step(c, c.step, mpi=mpi)
-    else:
-        scheme(c)
+    scheme(c)
 
 if __name__ == '__main__':
     main()
