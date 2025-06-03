f89f058acbd3cc70f401da11f553830a6b43ce5b
Yue Ying <hardyying@gmail.com>
2025-06-03 23:12:50 +0200

    fix nextsimdg restart

--- NEDAS/models/nextsim/dg/namelist.py
+++ NEDAS/models/nextsim/dg/namelist.py
@@ -17,7 +17,7 @@ def make_namelist(file_options:dict, model_config_file:str, ens_dir='.', **kwarg
 
     ##change the restart file name
     file_options_restart = file_options['restart']
-    fname_restart:str = restart.get_restart_filename(file_options_restart, ens_mem_id, time_start)
+    fname_restart:str = restart.get_restart_filename(file_options_restart, ens_mem_id, time)
     model_config['model']['init_file'] = os.path.basename(fname_restart)
     model_config['model']['start'] = time.strftime("%Y-%m-%dT%H:%M:%SZ")
     model_config['model']['stop'] = next_time.strftime("%Y-%m-%dT%H:%M:%SZ")
