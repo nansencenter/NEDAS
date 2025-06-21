2939a65ecbbdf08a9e5dc5dcad42af6f52a91591
Yue Ying <hardyying@gmail.com>
2025-06-21 23:14:27 +0200

    unittest for qcef assimilator

---
+++ tests/test_assimilator_qcef.py
@@ -0,0 +1,13 @@
+import unittest
+import importlib
+from NEDAS.assim_tools.assimilators.QCEF import QCEFAssimilator
+
+class TestQCEFAssimilator(unittest.TestCase):
+    def test_probit_transform(self):
+        pass
+
+    def test_obs_increment(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
