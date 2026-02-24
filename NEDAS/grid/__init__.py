be3282f2ab1ebabccd4dbf45ebe8020b58813d33
Yue Ying <hardyying@gmail.com>
2026-02-24 23:25:24 +0100

    fix GridType
    model classes can now use Generic to narrow down which grid type it uses.

--- NEDAS/grid/__init__.py
+++ NEDAS/grid/__init__.py
@@ -2,6 +2,8 @@ from NEDAS.grid.grid import Grid
 from NEDAS.grid.grid_regular import RegularGrid
 from NEDAS.grid.grid_irregular import IrregularGrid
 from NEDAS.grid.grid_1d import Grid1D
-from typing import Union
-GridType = Union[RegularGrid, IrregularGrid, Grid1D]
+
+from typing import Union, TypeVar
+GridType = TypeVar('GridType', bound=Union[RegularGrid, IrregularGrid, Grid1D])
+
 __all__ = ['Grid', 'RegularGrid', 'IrregularGrid', 'Grid1D', 'GridType']
