995e68a5389081addcffc81d7ad1343408fdc302
Timothy Smith <smith.timothy.andrew@gmail.com>
2026-07-03 14:23:46 +0200

    add olivia example

--- examples/qgmodel_benchmark/olivia/submit_job.sh
+++ examples/qgmodel_benchmark/olivia/submit_job.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+
 #SBATCH --account=nn2993k
 #SBATCH --job-name=run_expt
 #SBATCH --time=0-01:00:00
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=128
-#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=740G
+#SBATCH --qos=devel
+#SBATCH --partition=small
 #SBATCH --output=log/%x.out
 
-nens=100
-model=qg
-config_file=config/samples/$model.yml
+config_file=${PWD}/config.yml
+container_path=${PROJECT}/nedas-tutorials_latest.sif
 
-source $HOME/.bashrc
-source $HOME/python.src
+export APPTAINERENV_HYDRA_LAUNCHER=fork
 
-python -m NEDAS --config_file=$config_file --nproc=$SLURM_NTASKS --nens=$nens --work_dir=$SCRATCH/$model.n$nens
+apptainer exec \
+    $container_path \
+    python -m NEDAS --config_file=$config_file
